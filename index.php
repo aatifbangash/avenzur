d0aded8741dd3c30c446a9cedf7268dea6b47246
zahoor <zahoor@orangebuddies.pk>
2023-06-08 21:43:00 +0500

    Quotation Module Final

--- index.php
+++ index.php
@@ -40,9 +40,8 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	// define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	
-	if(!empty($_SERVER['HTTP_HOST']) && ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['HTTP_HOST'] == 'www.avenzur-pharma.com')) {
-		define('ENVIRONMENT', 'development');
+	if(!empty($_SERVER['HTTP_HOST']) && $_SERVER['HTTP_HOST'] == 'localhost') {
+		define('ENVIRONMENT', 'production');
 	} else {
 		define('ENVIRONMENT', 'production');
 	}
