473e5fe178a07984f6a74b4a319ccf501e714fbe
atif <atif@codenterprise.com>
2023-06-09 12:41:05 +0500

    c

--- app/config/routes.php
+++ app/config/routes.php
@@ -42,6 +42,7 @@ $route['reset_password/(:any)']  = 'main/reset_password/$1';
 
 // Admin area routes
 $route['admin']                      = 'admin/welcome';
+$route['alter_schema']               = 'admin/welcome/alter_schema';
 $route['admin/users']                = 'admin/auth/users';
 $route['admin/users/create_user']    = 'admin/auth/create_user';
 $route['admin/users/profile/(:num)'] = 'admin/auth/profile/$1';
