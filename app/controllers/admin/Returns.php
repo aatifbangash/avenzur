350261a7c7a380166ccf406da0ed7da337e4c8b0
Muhammadsaqb55 <79458134+Muhammadsaqb55@users.noreply.github.com>
2023-06-16 06:05:42 -0700

    add returns

--- app/controllers/admin/Returns.php
+++ app/controllers/admin/Returns.php
@@ -28,6 +28,196 @@ class Returns extends MY_Controller
         $this->data['logo']        = true;
     }
 
+    public function add_return()
+    {
+        $this->sma->checkPermissions();
+        $this->form_validation->set_message('is_natural_no_zero', lang('no_zero_required'));
+        $this->form_validation->set_rules('customer', lang('customer'), 'required');
+        $this->form_validation->set_rules('biller', lang('biller'), 'required');
+
+        if ($this->form_validation->run() == true) {
+            $date             = ($this->Owner || $this->Admin) ? $this->sma->fld(trim($this->input->post('date'))) : date('Y-m-d H:i:s');
+            $reference        = $this->input->post('reference_no') ? $this->input->post('reference_no') : $this->site->getReference('re');
+            $warehouse_id     = $this->input->post('warehouse');
+            $customer_id      = $this->input->post('customer');
+            $biller_id        = $this->input->post('biller');
+            $total_items      = $this->input->post('total_items');
+            $customer_details = $this->site->getCompanyByID($customer_id);
+            $customer         = !empty($customer_details->company) && $customer_details->company != '-' ? $customer_details->company : $customer_details->name;
+            $biller_details   = $this->site->getCompanyByID($biller_id);
+            $biller           = !empty($biller_details->company) && $biller_details->company != '-' ? $biller_details->company : $biller_details->name;
+            $note             = $this->sma->clear_tags($this->input->post('note'));
+            $staff_note       = $this->sma->clear_tags($this->input->post('staff_note'));
+            $shipping         = $this->input->post('shipping') ? $this->input->post('shipping') : 0;
+
+            $total            = 0;
+            $product_tax      = 0;
+            $product_discount = 0;
+            $gst_data         = [];
+            $total_cgst       = $total_sgst       = $total_igst       = 0;
+            $i                = isset($_POST['product_code']) ? sizeof($_POST['product_code']) : 0;
+
+
+            for ($r = 0; $r < $i; $r++) {
+                $item_id            = $_POST['product_id'][$r];
+                $item_type          = $_POST['product_type'][$r];
+                $item_code          = $_POST['product_code'][$r];
+                $item_name          = $_POST['product_name'][$r];
+                $item_option        = isset($_POST['product_option'][$r]) && $_POST['product_option'][$r] != 'false' && $_POST['product_option'][$r] != 'null' ? $_POST['product_option'][$r] : null;
+                $real_unit_price    = $this->sma->formatDecimal($_POST['real_unit_price'][$r]);
+                $unit_price         = $this->sma->formatDecimal($_POST['unit_price'][$r]);
+                $item_unit_quantity = $_POST['quantity'][$r];
+                $item_serial        = $_POST['serial'][$r]           ?? '';
+                $item_tax_rate      = $_POST['product_tax'][$r]      ?? null;
+                $item_discount      = $_POST['product_discount'][$r] ?? null;
+                $item_unit          = $_POST['product_unit'][$r];
+                $item_quantity      = $_POST['product_base_quantity'][$r];
+
+                if (isset($item_code) && isset($real_unit_price) && isset($unit_price) && isset($item_quantity)) {
+                    $product_details  = $item_type != 'manual' ? $this->site->getProductByCode($item_code) : null;
+                    $pr_discount      = $this->site->calculateDiscount($item_discount, $unit_price);
+                    $unit_price       = $this->sma->formatDecimal($unit_price - $pr_discount);
+                    $item_net_price   = $unit_price;
+                    $pr_item_discount = $this->sma->formatDecimal($pr_discount * $item_unit_quantity);
+                    $product_discount += $pr_item_discount;
+                    $pr_item_tax = $item_tax = 0;
+                    $tax         = '';
+
+                    if (isset($item_tax_rate) && $item_tax_rate != 0) {
+                        $tax_details = $this->site->getTaxRateByID($item_tax_rate);
+                        $ctax        = $this->site->calculateTax($product_details, $tax_details, $unit_price);
+                        $item_tax    = $this->sma->formatDecimal($ctax['amount']);
+                        $tax         = $ctax['tax'];
+                        if (!$product_details || (!empty($product_details) && $product_details->tax_method != 1)) {
+                            $item_net_price = $unit_price - $item_tax;
+                        }
+                        $pr_item_tax = $this->sma->formatDecimal(($item_tax * $item_unit_quantity), 4);
+                        if ($this->Settings->indian_gst && $gst_data = $this->gst->calculateIndianGST($pr_item_tax, ($biller_details->state == $customer_details->state), $tax_details)) {
+                            $total_cgst += $gst_data['cgst'];
+                            $total_sgst += $gst_data['sgst'];
+                            $total_igst += $gst_data['igst'];
+                        }
+                    }
+
+                    $product_tax += $pr_item_tax;
+                    $subtotal = (($item_net_price * $item_unit_quantity) + $pr_item_tax);
+                    $unit     = $this->site->getUnitByID($item_unit);
+
+                    $product = [
+                        'product_id'        => $item_id,
+                        'product_code'      => $item_code,
+                        'product_name'      => $item_name,
+                        'product_type'      => $item_type,
+                        'option_id'         => $item_option,
+                        'net_unit_price'    => $item_net_price,
+                        'unit_price'        => $this->sma->formatDecimal($item_net_price + $item_tax),
+                        'quantity'          => $item_quantity,
+                        'product_unit_id'   => $unit ? $unit->id : null,
+                        'product_unit_code' => $unit ? $unit->code : null,
+                        'unit_quantity'     => $item_unit_quantity,
+                        'warehouse_id'      => $warehouse_id,
+                        'item_tax'          => $pr_item_tax,
+                        'tax_rate_id'       => $item_tax_rate,
+                        'tax'               => $tax,
+                        'discount'          => $item_discount,
+                        'item_discount'     => $pr_item_discount,
+                        'subtotal'          => $this->sma->formatDecimal($subtotal),
+                        'serial_no'         => $item_serial,
+                        'real_unit_price'   => $real_unit_price,
+                    ];
+
+                    $products[] = ($product + $gst_data);
+                    $total += $this->sma->formatDecimal(($item_net_price * $item_unit_quantity), 4);
+                }
+            }
+            if (empty($products)) {
+                $this->form_validation->set_rules('product', lang('order_items'), 'required');
+            } else {
+                krsort($products);
+            }
+
+            $order_discount = $this->site->calculateDiscount($this->input->post('order_discount'), ($total + $product_tax), true);
+            $total_discount = $this->sma->formatDecimal(($order_discount + $product_discount), 4);
+            $order_tax      = $this->site->calculateOrderTax($this->input->post('order_tax'), ($total + $product_tax - $order_discount));
+            $total_tax      = $this->sma->formatDecimal(($product_tax + $order_tax), 4);
+            $grand_total    = $this->sma->formatDecimal(($total + $total_tax + $this->sma->formatDecimal($shipping) - $this->sma->formatDecimal($order_discount)), 4);
+            $data           = [
+                'date'              => $date,
+                'reference_no'      => $reference,
+                'customer_id'       => $customer_id,
+                'customer'          => $customer,
+                'biller_id'         => $biller_id,
+                'biller'            => $biller,
+                'warehouse_id'      => $warehouse_id,
+                'note'              => $note,
+                'staff_note'        => $staff_note,
+                'total'             => $total,
+                'product_discount'  => $product_discount,
+                'order_discount_id' => $this->input->post('order_discount'),
+                'order_discount'    => $order_discount,
+                'total_discount'    => $total_discount,
+                'product_tax'       => $product_tax,
+                'order_tax_id'      => $this->input->post('order_tax'),
+                'order_tax'         => $order_tax,
+                'total_tax'         => $total_tax,
+                'grand_total'       => $grand_total,
+                'total_items'       => $total_items,
+                'paid'              => 0,
+                'created_by'        => $this->session->userdata('user_id'),
+                'hash'              => hash('sha256', microtime() . mt_rand()),
+            ];
+            if ($this->Settings->indian_gst) {
+                $data['cgst'] = $total_cgst;
+                $data['sgst'] = $total_sgst;
+                $data['igst'] = $total_igst;
+            }
+
+            if ($_FILES['document']['size'] > 0) {
+                $this->load->library('upload');
+                $config['upload_path']   = $this->digital_upload_path;
+                $config['allowed_types'] = $this->digital_file_types;
+                $config['max_size']      = $this->allowed_file_size;
+                $config['overwrite']     = false;
+                $config['encrypt_name']  = true;
+                $this->upload->initialize($config);
+                if (!$this->upload->do_upload('document')) {
+                    $error = $this->upload->display_errors();
+                    $this->session->set_flashdata('error', $error);
+                    redirect($_SERVER['HTTP_REFERER']);
+                }
+                $photo              = $this->upload->file_name;
+                $data['attachment'] = $photo;
+            }
+
+            // $this->sma->print_arrays($data, $products);
+        }
+
+        if ($this->form_validation->run() == true && $this->returns_model->addReturn($data, $products)) {
+            //$return_insert_id = $this->db->insert_id();
+            //$this->returns_model->convert_return_invoice($return_insert_id, $products);
+
+            $this->session->set_userdata('remove_rels', 1);
+            $this->session->set_flashdata('message', lang('return_added'));
+            admin_redirect('returns');
+        } else {
+            $this->data['error']      = (validation_errors() ? validation_errors() : $this->session->flashdata('error'));
+            $this->data['billers']    = $this->site->getAllCompanies('biller');
+            $this->data['warehouses'] = $this->site->getAllWarehouses();
+            $this->data['tax_rates']  = $this->site->getAllTaxRates();
+            $this->data['units']      = $this->site->getAllBaseUnits();
+
+            // $user         = $this->site->getUser();
+            // $group_id = $user->group_id;
+
+            $this->db->select('id,name')->from('companies')->where('group_name','supplier');
+            $this->data['suppliers'] =$this->db->get()->result();
+            
+            $bc                       = [['link' => base_url(), 'page' => lang('home')], ['link' => admin_url('returns'), 'page' => lang('returns')], ['link' => '#', 'page' => lang('add_return')]];
+            $meta                     = ['page_title' => lang('add_return'), 'bc' => $bc];
+            $this->page_construct('returns/add_supplier', $meta, $this->data);
+        }
+    }
+
     public function add()
     {
         $this->sma->checkPermissions();
@@ -36,6 +226,9 @@ class Returns extends MY_Controller
         $this->form_validation->set_rules('biller', lang('biller'), 'required');
 
         if ($this->form_validation->run() == true) {
+
+            $return_screen     = $this->input->post('return_screen');
+
             $date             = ($this->Owner || $this->Admin) ? $this->sma->fld(trim($this->input->post('date'))) : date('Y-m-d H:i:s');
             $reference        = $this->input->post('reference_no') ? $this->input->post('reference_no') : $this->site->getReference('re');
             $warehouse_id     = $this->input->post('warehouse');
@@ -293,6 +486,8 @@ class Returns extends MY_Controller
         }
     }
 
+    
+
     public function edit($id = null)
     {
         $this->sma->checkPermissions();
@@ -544,12 +739,12 @@ class Returns extends MY_Controller
 
     public function getReturns($warehouse_id = null)
     {
-        $this->sma->checkPermissions('index');
-
-        if ((!$this->Owner || !$this->Admin) && !$warehouse_id) {
+       
+        // $this->sma->checkPermissions('index');
+        // if ((!$this->Owner || !$this->Admin) && !$warehouse_id) {
             $user         = $this->site->getUser();
             $warehouse_id = $user->warehouse_id;
-        }
+        // }
 
         $this->load->library('datatables');
         if ($warehouse_id) {
@@ -597,7 +792,6 @@ class Returns extends MY_Controller
         if (strlen($term) < 1 || !$term) {
             die("<script type='text/javascript'>setTimeout(function(){ window.top.location.href = '" . admin_url('welcome') . "'; }, 10);</script>");
         }
-
         $analyzed  = $this->sma->analyze_term($term);
         $sr        = $analyzed['term'];
         $option_id = $analyzed['option_id'];
@@ -607,6 +801,7 @@ class Returns extends MY_Controller
         $bprice    = $strict ? null : $analyzed['price']    ?? null;
 
         $rows = $this->returns_model->getProductNames($sr);
+
         if ($rows) {
             $r = 0;
             foreach ($rows as $row) {
@@ -615,6 +810,7 @@ class Returns extends MY_Controller
                 unset($row->cost, $row->details, $row->product_details, $row->image, $row->barcode_symbology, $row->cf1, $row->cf2, $row->cf3, $row->cf4, $row->cf5, $row->cf6, $row->supplier1price, $row->supplier2price, $row->cfsupplier3price, $row->supplier4price, $row->supplier5price, $row->supplier1, $row->supplier2, $row->supplier3, $row->supplier4, $row->supplier5, $row->supplier1_part_no, $row->supplier2_part_no, $row->supplier3_part_no, $row->supplier4_part_no, $row->supplier5_part_no);
                 $row->item_tax_method = $row->tax_method;
                 $options              = $this->returns_model->getProductOptions($row->id);
+
                 if ($options) {
                     $opt = $option_id && $r == 0 ? $this->returns_model->getProductOptionByID($option_id) : current($options);
                     if (!$option_id || $r > 0) {
@@ -623,12 +819,24 @@ class Returns extends MY_Controller
                 } else {
                     $opt        = json_decode('{}');
                     $opt->price = 0;
+                    $opt->cost = 0;
                     $option_id  = false;
                 }
+
+
                 $row->option = $option_id;
                 if ($row->promotion) {
                     $row->price = $row->promo_price;
                 }
+
+
+                
+                 $row->cost_price = $opt->cost;
+               
+                 print_r($row->cost_price);
+
+                // $row->cost       = $row->cost;
+
                 $row->base_quantity   = 1;
                 $row->base_unit       = $row->unit;
                 $row->real_unit_price = $row->price;
@@ -638,6 +846,10 @@ class Returns extends MY_Controller
                 $row->discount        = '0';
                 $row->serial          = '';
                 $row->comment         = '';
+                $row->batch_no        = '';
+                $row->expiry          = '';
+                $row->bonus           = '0';
+                
                 $combo_items          = false;
                 if ($row->type == 'combo') {
                     $combo_items = $this->site->getProductComboItems($row->id);
@@ -645,7 +857,7 @@ class Returns extends MY_Controller
                 $row->qty = $qty ? $qty : ($bprice ? $bprice / $row->price : 1);
                 $units    = $this->site->getUnitsByBUID($row->base_unit);
                 $tax_rate = $this->site->getTaxRateByID($row->tax_rate);
-
+                // $row->batch_no = $row->batchno;
                 $pr[] = ['id' => sha1($c . $r), 'item_id' => $row->id, 'label' => $row->name . ' (' . $row->code . ')',
                     'row'     => $row, 'tax_rate' => $tax_rate, 'units' => $units, 'options' => $options, ];
                 $r++;
