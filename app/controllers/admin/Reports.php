138e9acf7ba7c5319637c8362637dfd6f93f4b81
Asif Khan <asif@codenterprise.com>
2023-07-06 23:10:53 +0500

    c

--- app/controllers/admin/Reports.php
+++ app/controllers/admin/Reports.php
@@ -3549,26 +3549,38 @@ class Reports extends MY_Controller
         $this->data['incentives']= "incentive";
         $this->data['users']      = $this->reports_model->getStaff();
 
-
         $user         = $this->input->post('user');
         $start_date   = $this->input->post('start_date');
         $end_date     = $this->input->post('end_date');
-       
-        if ($start_date) {
-            $start_date = $this->sma->fld($start_date);
-            $end_date   = $this->sma->fld($end_date);
-        }
-      
-     
+        if(!empty($user)){
+                   
+            if ($start_date) {
+                $start_date = $this->sma->fld($start_date);
+                $end_date   = $this->sma->fld($end_date);
+            }
+        
+        
 
-        $this->db->select('p.*,SUM(t.quantity) as total_quantity,SUM(t.net_unit_price) as total_price',false)
-        ->from('sale_items t')
-        ->join('products p','p.id = t.product_id','full')
-        ->join('sales s','s.id = t.sale_id','full')
-        ->where('s.created_by', $user)
-        // ->where($this->db->dbprefix('sales') . '.date BETWEEN "' . $start_date . '" and "' . $end_date . '"')
-        ->group_by('t.product_id');
-        $this->data['products'] = $this->db->get()->result();
+            $this->db->select('p.*,SUM(t.quantity) as total_quantity,SUM(t.net_unit_price) as total_price',false)
+            ->from('sale_items t')
+            ->join('products p','p.id = t.product_id','full')
+            ->join('sales s','s.id = t.sale_id','full')
+            ->where('s.created_by', $user)
+            ->where('p.incentive_qty IS NOT NULL', NULL)
+            ->group_by('t.product_id');
+
+            
+            if ($start_date && $start_date != "0000-00-00 00:00:00") {
+                $this->db->where('s.date >=', $start_date);
+            
+
+                if ($end_date  && $end_date != "0000-00-00 00:00:00") {
+                    $this->db->where('s.date <=', $end_date);
+                }
+            }
+            $this->data['products'] = $this->db->get()->result();
+            
+        }
 
         $this->data['error']  = (validation_errors()) ? validation_errors() : $this->session->flashdata('error');
         $bc                   = [['link' => base_url(), 'page' => lang('home')], ['link' => admin_url('reports'), 'page' => lang('reports')], ['link' => '#', 'page' => lang('staff_report')]];
--- themes/blue/admin/views/reports/incentives.php
+++ themes/blue/admin/views/reports/incentives.php
@@ -36,31 +36,55 @@ if ($this->input->post('end_date')) {
 ?>
 
 <script>
-    $(document).ready(function () {
+    $(document).ready(function() {
         oTable = $('#SlRData').dataTable({
-            "aaSorting": [[0, "desc"]],
-            "aLengthMenu": [[10, 25, 50, 100, -1], [10, 25, 50, 100, "<?= lang('all') ?>"]],
+            "aaSorting": [
+                [0, "desc"]
+            ],
+            "aLengthMenu": [
+                [10, 25, 50, 100, -1],
+                [10, 25, 50, 100, "<?= lang('all') ?>"]
+            ],
             "iDisplayLength": <?= $Settings->rows_per_page ?>,
-            'bProcessing': true, 'bServerSide': true,
+            'bProcessing': true,
+            'bServerSide': true,
             'sAjaxSource': '<?= admin_url('reports/getSalesReport/?v=1' . $v) ?>',
-            'fnServerData': function (sSource, aoData, fnCallback) {
+            'fnServerData': function(sSource, aoData, fnCallback) {
                 aoData.push({
                     "name": "<?= $this->security->get_csrf_token_name() ?>",
                     "value": "<?= $this->security->get_csrf_hash() ?>"
                 });
-                $.ajax({'dataType': 'json', 'type': 'POST', 'url': sSource, 'data': aoData, 'success': fnCallback});
+                $.ajax({
+                    'dataType': 'json',
+                    'type': 'POST',
+                    'url': sSource,
+                    'data': aoData,
+                    'success': fnCallback
+                });
             },
-            'fnRowCallback': function (nRow, aData, iDisplayIndex) {
+            'fnRowCallback': function(nRow, aData, iDisplayIndex) {
                 nRow.id = aData[9];
                 nRow.className = (aData[5] > 0) ? "invoice_link2" : "invoice_link2 warning";
                 return nRow;
             },
-            "aoColumns": [{"mRender": fld}, null, null, null, {
+            "aoColumns": [{
+                "mRender": fld
+            }, null, null, null, {
                 "bSearchable": false,
                 "mRender": pqFormat
-            }, {"mRender": currencyFormat}, {"mRender": currencyFormat}, {"mRender": currencyFormat}, {"mRender": row_status}],
-            "fnFooterCallback": function (nRow, aaData, iStart, iEnd, aiDisplay) {
-                var gtotal = 0, paid = 0, balance = 0;
+            }, {
+                "mRender": currencyFormat
+            }, {
+                "mRender": currencyFormat
+            }, {
+                "mRender": currencyFormat
+            }, {
+                "mRender": row_status
+            }],
+            "fnFooterCallback": function(nRow, aaData, iStart, iEnd, aiDisplay) {
+                var gtotal = 0,
+                    paid = 0,
+                    balance = 0;
                 for (var i = 0; i < aaData.length; i++) {
                     gtotal += parseFloat(aaData[aiDisplay[i]][5]);
                     paid += parseFloat(aaData[aiDisplay[i]][6]);
@@ -71,61 +95,93 @@ if ($this->input->post('end_date')) {
                 nCells[6].innerHTML = currencyFormat(parseFloat(paid));
                 nCells[7].innerHTML = currencyFormat(parseFloat(balance));
             }
-        }).fnSetFilteringDelay().dtFilter([
-            {column_number: 0, filter_default_label: "[<?=lang('date');?> (yyyy-mm-dd)]", filter_type: "text", data: []},
-            {column_number: 1, filter_default_label: "[<?=lang('reference_no');?>]", filter_type: "text", data: []},
-            {column_number: 2, filter_default_label: "[<?=lang('biller');?>]", filter_type: "text", data: []},
-            {column_number: 3, filter_default_label: "[<?=lang('customer');?>]", filter_type: "text", data: []},
-            {column_number: 8, filter_default_label: "[<?=lang('payment_status');?>]", filter_type: "text", data: []},
+        }).fnSetFilteringDelay().dtFilter([{
+                column_number: 0,
+                filter_default_label: "[<?= lang('date'); ?> (yyyy-mm-dd)]",
+                filter_type: "text",
+                data: []
+            },
+            {
+                column_number: 1,
+                filter_default_label: "[<?= lang('reference_no'); ?>]",
+                filter_type: "text",
+                data: []
+            },
+            {
+                column_number: 2,
+                filter_default_label: "[<?= lang('biller'); ?>]",
+                filter_type: "text",
+                data: []
+            },
+            {
+                column_number: 3,
+                filter_default_label: "[<?= lang('customer'); ?>]",
+                filter_type: "text",
+                data: []
+            },
+            {
+                column_number: 8,
+                filter_default_label: "[<?= lang('payment_status'); ?>]",
+                filter_type: "text",
+                data: []
+            },
         ], "footer");
     });
 </script>
 <script type="text/javascript">
-    $(document).ready(function () {
+    $(document).ready(function() {
         $('#form').hide();
         <?php if ($this->input->post('customer')) {
-    ?>
-        $('#customer').val(<?= $this->input->post('customer') ?>).select2({
-            minimumInputLength: 1,
-            data: [],
-            initSelection: function (element, callback) {
-                $.ajax({
-                    type: "get", async: false,
-                    url: site.base_url + "customers/suggestions/" + $(element).val(),
-                    dataType: "json",
-                    success: function (data) {
-                        callback(data.results[0]);
-                    }
-                });
-            },
-            ajax: {
-                url: site.base_url + "customers/suggestions",
-                dataType: 'json',
-                quietMillis: 15,
-                data: function (term, page) {
-                    return {
-                        term: term,
-                        limit: 10
-                    };
+        ?>
+            $('#customer').val(<?= $this->input->post('customer') ?>).select2({
+                minimumInputLength: 1,
+                data: [],
+                initSelection: function(element, callback) {
+                    $.ajax({
+                        type: "get",
+                        async: false,
+                        url: site.base_url + "customers/suggestions/" + $(element).val(),
+                        dataType: "json",
+                        success: function(data) {
+                            callback(data.results[0]);
+                        }
+                    });
                 },
-                results: function (data, page) {
-                    if (data.results != null) {
-                        return {results: data.results};
-                    } else {
-                        return {results: [{id: '', text: 'No Match Found'}]};
+                ajax: {
+                    url: site.base_url + "customers/suggestions",
+                    dataType: 'json',
+                    quietMillis: 15,
+                    data: function(term, page) {
+                        return {
+                            term: term,
+                            limit: 10
+                        };
+                    },
+                    results: function(data, page) {
+                        if (data.results != null) {
+                            return {
+                                results: data.results
+                            };
+                        } else {
+                            return {
+                                results: [{
+                                    id: '',
+                                    text: 'No Match Found'
+                                }]
+                            };
+                        }
                     }
                 }
-            }
-        });
+            });
 
-        $('#customer').val(<?= $this->input->post('customer') ?>);
+            $('#customer').val(<?= $this->input->post('customer') ?>);
         <?php
-} ?>
-        $('.toggle_down').click(function () {
+        } ?>
+        $('.toggle_down').click(function() {
             $("#form").slideDown();
             return false;
         });
-        $('.toggle_up').click(function () {
+        $('.toggle_up').click(function() {
             $("#form").slideUp();
             return false;
         });
@@ -136,40 +192,13 @@ if ($this->input->post('end_date')) {
 <div class="box">
     <div class="box-header">
         <h2 class="blue"><i class="fa-fw fa fa-heart"></i><?= lang('Pharmacist_Incentives_Report'); ?> <?php
-            if ($this->input->post('start_date')) {
-                echo 'From ' . $this->input->post('start_date') . ' to ' . $this->input->post('end_date');
-            }
-            ?>
+                                                                                                        if ($this->input->post('start_date')) {
+                                                                                                            echo 'From ' . $this->input->post('start_date') . ' to ' . $this->input->post('end_date');
+                                                                                                        }
+                                                                                                        ?>
         </h2>
 
-        <div class="box-icon">
-            <ul class="btn-tasks">
-                <li class="dropdown">
-                    <a href="#" class="toggle_up tip" title="<?= lang('hide_form') ?>">
-                        <i class="icon fa fa-toggle-up"></i>
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" class="toggle_down tip" title="<?= lang('show_form') ?>">
-                        <i class="icon fa fa-toggle-down"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div class="box-icon">
-            <ul class="btn-tasks">
-                <li class="dropdown">
-                    <a href="#" id="xls" class="tip" title="<?= lang('download_xls') ?>">
-                        <i class="icon fa fa-file-excel-o"></i>
-                    </a>
-                </li>
-                <li class="dropdown">
-                    <a href="#" id="image" class="tip" title="<?= lang('save_image') ?>">
-                        <i class="icon fa fa-file-picture-o"></i>
-                    </a>
-                </li>
-            </ul>
-        </div>
+        
     </div>
     <div class="box-content">
         <div class="row">
@@ -179,21 +208,21 @@ if ($this->input->post('end_date')) {
 
                 <div>
 
-                 
+
                     <?php
                     $attrib = ['data-toggle' => 'validator', 'role' => 'form'];
                     echo admin_form_open_multipart('reports/getIncentives', $attrib);
                     ?>
-                
+
                     <div class="row">
-                       
-                    <div class="col-sm-4">
+
+                        <div class="col-sm-4">
                             <div class="form-group">
                                 <?= lang('start_date', 'start_date'); ?>
                                 <?php echo form_input('start_date', (isset($_POST['start_date']) ? $_POST['start_date'] : ''), 'class="form-control datetime" id="start_date"'); ?>
                             </div>
                         </div>
-                        
+
                         <div class="col-sm-4">
                             <div class="form-group">
                                 <?= lang('end_date', 'end_date'); ?>
@@ -216,8 +245,7 @@ if ($this->input->post('end_date')) {
 
                     </div>
                     <div class="form-group">
-                        <div
-                           <button class="btn btn-primary" name="submit" type="submit">Submit</button>
+                        <div <button class="btn btn-primary" name="submit" type="submit">Submit</button>
                         </div>
                     </div>
                     <?php echo form_close(); ?>
@@ -226,74 +254,73 @@ if ($this->input->post('end_date')) {
                 <div class="clearfix"></div>
 
                 <div class="table-responsive">
-                    <table id="" cellpadding="0" cellspacing="0" border="0"
-                           class="table table-bordered table-hover table-striped ">
+                    <table id="" cellpadding="0" cellspacing="0" border="0" class="table table-bordered table-hover table-striped ">
                         <thead>
-                        <tr style="text-align:center;">
-                            <th><?= lang('Item code'); ?></th>
-                            <th><?= lang('Item name'); ?></th>
-                            <th><?= lang('Incentive Qty'); ?></th>
-                            <th><?= lang('Incentive value'); ?></th>
-                            <th><?= lang('Total Quantity'); ?></th>
-                            <th><?= lang('Last selling price'); ?></th>
-                            <th><?= lang('Incentive selling value'); ?></th>
-                            <!-- <th><?= lang('From date'); ?></th>
+                            <tr style="text-align:center;">
+                                <th><?= lang('Item code'); ?></th>
+                                <th><?= lang('Item name'); ?></th>
+                                <th><?= lang('Incentive Qty'); ?></th>
+                                <th><?= lang('Incentive value'); ?></th>
+                                <th><?= lang('Total Quantity'); ?></th>
+                                <th><?= lang('Last selling price'); ?></th>
+                                <th><?= lang('Incentive selling value'); ?></th>
+                                <!-- <th><?= lang('From date'); ?></th>
                             <th><?= lang('To date'); ?></th>  -->
-                        </tr>
+                            </tr>
                         </thead>
                         <tbody style="text-align:center;">
-                        <?php
-                             if($incentives !="incentives"){
+                            <?php
+                            if ($incentives != "incentives") {
                                 $sp[''] = '';
                                 foreach ($products as $product) {
-                                    $values=  $product->incentive_value;
-                                    $getLast = $values[strlen($values)-1];
+                                    $values =  $product->incentive_value;
+                                    $getLast = $values[strlen($values) - 1];
                                     // echo $getLast; 
-
-                                    if($product->incentive_value <= round($product->total_quantity)){
-                                    if($getLast == "%"){
-                                    $removeLast = rtrim($values, "%");
-                                    $incentive =  ($removeLast/100)*($product->total_price);
-                                    $incentivePrice = round($incentive);
-                                    }else{
-                                    $incentive = ($product->incentive_value)/($product->incentive_qty);
-                                    $incentivePrice = round(($incentive)*($product->total_quantity));
-                                    }
-                                    }else{
-                                    $incentivePrice = 0;
-                                    }
-                                   echo "<tr><td>$product->code</td><td>$product->name</td><td>$product->incentive_qty</td><td>$product->incentive_value</td><td>$product->total_quantity</td><td>$product->total_price</td><td>$incentivePrice</td></tr>";
-                                }
-                             }
                                 
-                         ?> 
+                                    if ($product->incentive_value <= round($product->total_quantity)) {
+                                        if ($getLast == "%") {
+                                            $removeLast = rtrim($values, "%");
+                                            $incentive =  ($removeLast / 100) * ($product->total_price);
+                                            $incentivePrice = round($incentive);
+                                        } else {
+                                            $incentive = ($product->incentive_value) / ($product->incentive_qty);
+                                            $incentivePrice = round(($incentive) * ($product->total_quantity));
+                                        }
+                                    } else {
+                                        $incentivePrice = 0;
+                                    }
+                                    echo "<tr><td>$product->code</td><td>$product->name</td><td>$product->incentive_qty</td><td>$product->incentive_value</td><td>$product->total_quantity</td><td>$product->total_price</td><td>$incentivePrice</td></tr>";
+                                }
+                            }
+
+                            ?>
 
                         </tbody>
                     </table>
                 </div>
-                    
-                </div>
+
             </div>
         </div>
     </div>
 </div>
+</div>
 <script type="text/javascript" src="<?= $assets ?>js/html2canvas.min.js"></script>
 <script type="text/javascript">
-    $(document).ready(function () {
-        $('#pdf').click(function (event) {
+    $(document).ready(function() {
+        $('#pdf').click(function(event) {
             event.preventDefault();
-            window.location.href = "<?=admin_url('reports/getSalesReport/pdf/?v=1' . $v)?>";
+            window.location.href = "<?= admin_url('reports/getSalesReport/pdf/?v=1' . $v) ?>";
             return false;
         });
-        $('#xls').click(function (event) {
+        $('#xls').click(function(event) {
             event.preventDefault();
-            window.location.href = "<?=admin_url('reports/getSalesReport/0/xls/?v=1' . $v)?>";
+            window.location.href = "<?= admin_url('reports/getSalesReport/0/xls/?v=1' . $v) ?>";
             return false;
         });
-        $('#image').click(function (event) {
+        $('#image').click(function(event) {
             event.preventDefault();
             html2canvas($('.box'), {
-                onrendered: function (canvas) {
+                onrendered: function(canvas) {
                     openImg(canvas.toDataURL());
                 }
             });
