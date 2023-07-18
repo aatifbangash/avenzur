ba78426ddfb7fdb0f857de52289960cd81f157d4
faisal abbas <faisalabb67@gmail.com>
2023-07-18 19:05:55 +0500

    'c'

--- app/controllers/admin/Purchases.php
+++ app/controllers/admin/Purchases.php
@@ -1604,11 +1604,11 @@ class Purchases extends MY_Controller
     public function index($warehouse_id = null)
     {
         $this->sma->checkPermissions();
+        
         $this->data['error'] = (validation_errors()) ? validation_errors() : $this->session->flashdata('error');
-        if ($this->Owner || $this->Admin || !$this->session->userdata('warehouse_id')) {
+        if ((!$this->Owner && !$this->Admin) || !$this->session->userdata('warehouse_id')) {
             $this->data['warehouses']   = $this->site->getAllWarehouses();
             $this->data['warehouse_id'] = $warehouse_id;
-            echo 'warehouseid: '.$warehouse_id;exit;
             $this->data['warehouse']    = $warehouse_id ? $this->site->getWarehouseByID($warehouse_id) : null;
         } else {
             $this->data['warehouses']   = null;
