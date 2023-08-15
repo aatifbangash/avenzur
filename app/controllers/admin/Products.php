5cb635bfd07e5529e1dceade42c53deffc9e1946
faisal abbas <faisalabb67@gmail.com>
2023-08-15 16:52:32 +0500

    'c'

--- app/controllers/admin/Products.php
+++ app/controllers/admin/Products.php
@@ -1607,7 +1607,7 @@ class Products extends MY_Controller
                 ->join('units', 'products.unit=units.id', 'left')
                 ->join('brands', 'products.brand=brands.id', 'left')
                 ->group_by('products.id');*/
-            echo 'Here in else block...';exit;
+
             $this->datatables
                 ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code,{$this->db->dbprefix('products')}.sequence_code as sequence_code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, COALESCE(wp.quantity, 0) as quantity, {$this->db->dbprefix('units')}.code as unit, wp.rack as rack, alert_quantity", false)
                 ->from('products');
@@ -1622,7 +1622,7 @@ class Products extends MY_Controller
                 $this->datatables->join('categories', 'products.category_id=categories.id', 'left')
                 ->join('units', 'products.unit=units.id', 'left')
                 ->join('brands', 'products.brand=brands.id', 'left')
-                ->group_by('products.id');;
+                ->group_by('products.id');
 
         }
         if (!$this->Owner && !$this->Admin) {
