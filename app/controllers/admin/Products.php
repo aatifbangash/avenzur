f888bbd977194eb9a4549457a21ccbf486cb827b
faisal abbas <faisalabb67@gmail.com>
2023-08-15 17:40:11 +0500

    'c'

--- app/controllers/admin/Products.php
+++ app/controllers/admin/Products.php
@@ -1612,7 +1612,7 @@ class Products extends MY_Controller
                 ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code,{$this->db->dbprefix('products')}.sequence_code as sequence_code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, COALESCE(wp.quantity, 0) as quantity, {$this->db->dbprefix('units')}.code as unit, wp.rack as rack, alert_quantity", false)
                 ->from('products');
                 if ($this->Settings->display_all_products) {
-                    $this->datatables->join('warehouses_products wp', "wp.product_id=products.id AND wp.warehouse_id={$warehouse_id}", 'left');
+                    $this->datatables->join('warehouses_products wp', "wp.product_id=products.id", 'left');
                 // $this->datatables->join("( SELECT product_id, quantity, rack from {$this->db->dbprefix('warehouses_products')} WHERE warehouse_id = {$warehouse_id}) wp", 'products.id=wp.product_id', 'left');
                 } else {
                     $this->datatables->join('warehouses_products wp', 'products.id=wp.product_id', 'left')
