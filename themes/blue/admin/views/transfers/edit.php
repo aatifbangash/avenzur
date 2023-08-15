57bb5915be5b028cb76c90b91b0e9b824f0e45bf
faisal abbas <faisalabb67@gmail.com>
2023-08-15 12:46:06 +0500

    'c'

--- themes/blue/admin/views/transfers/edit.php
+++ themes/blue/admin/views/transfers/edit.php
@@ -320,7 +320,7 @@
                                             ?>
                                             <th class="col-md-1"><?= lang('net_unit_cost'); ?></th>
                                             <th class="col-md-1"><?= lang('quantity'); ?></th>
-                                            
+                                            <th class="col-md-1"><?= lang('Actual Quantity'); ?></th>
                                             <?php
                                             if ($Settings->tax1) {
                                                 echo '<th class="col-md-1">' . $this->lang->line('product_tax') . '</th>';
