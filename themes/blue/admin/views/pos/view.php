61393404077766f69214c956726b9be0a7549b1d
Faisal Abbas <faisalabbas67@gmail.com>
2023-08-26 13:13:10 +0300

    'c'

--- themes/blue/admin/views/pos/view.php
+++ themes/blue/admin/views/pos/view.php
@@ -410,7 +410,7 @@
                 </span>
                 <span class="pull-left col-xs-12">
                         <?php
-                            echo '<button onclick="print_instructions(\'' . addslashes($inv->instructions) . '\');" class="btn btn-block btn-primary">' . lang('Print Instructions') . '</button>';
+                            echo '<button onclick="print_instructions(' . json_encode($inv->instructions) . ');" class="btn btn-block btn-primary">' . lang('Print Instructions') . '</button>';
                         ?>
                 </span>
                 <span class="pull-left col-xs-12"><a class="btn btn-block btn-success" href="#" id="email"><?= lang('email'); ?></a></span>
