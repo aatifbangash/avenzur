93e2ba27171729407f43048a6cb2b45b91a4a3ea
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-13 13:54:27 +0300

    'c'

--- themes/blue/admin/views/pos/view.php
+++ themes/blue/admin/views/pos/view.php
@@ -514,7 +514,7 @@
                     printWindow.document.write('</body></html>');
 
                     // Add CSS styles for better formatting (optional)
-                    printWindow.document.write('<style>body{font-family: Arial, sans-serif;} b{font-size: 18px;} </style>');
+                    printWindow.document.write('<style>body{font-family: Arial, sans-serif;} b{font-size: 16px;} </style>');
                     
                     printWindow.print();
                     printWindow.close();
