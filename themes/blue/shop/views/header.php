6cab2763f8df474f28a0892025e50e1cac9a7577
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-17 09:35:24 +0300

    'c'

--- themes/blue/shop/views/header.php
+++ themes/blue/shop/views/header.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google-site-verification" content="UfVhqcmiHjWujvtylPvb9EH_m0yocS-R784juDf-Rcg" />
     <script type="text/javascript">if (parent.frames.length !== 0) { top.location = '<?= site_url(); ?>'; }</script>
     <title><?= $page_title; ?></title>
     <meta name="description" content="<?= $page_desc; ?>">
