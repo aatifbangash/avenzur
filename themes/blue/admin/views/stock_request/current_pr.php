5030f67a1366edb59cb2ab8615f8d9951d0dfa7f
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-20 16:36:07 +0300

    'c'

--- themes/blue/admin/views/stock_request/current_pr.php
+++ themes/blue/admin/views/stock_request/current_pr.php
@@ -38,7 +38,9 @@
                                     $wh[$warehouse->id] = $warehouse->name;
                                 }
                                 echo form_dropdown('warehouse', $wh, ($_POST['warehouse'] ?? $Settings->default_warehouse), 'id="powarehouse" class="form-control input-tip select" data-placeholder="' . lang('select') . ' ' . lang('warehouse') . '" required="required" style="width:100%;" '); ?>
-                        </div>
+                            
+                            <input type="submit" value="search" class="btn btn-primary" name="search_product" />    
+                    </div>
                     </div>
 
                     <div class="col-md-4">
