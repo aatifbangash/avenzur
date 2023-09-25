da1361fd8c8e9200b69a1a3bbfbd5d0c0e491d16
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-25 12:15:05 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -717,8 +717,8 @@
             </div>
 
             <div class="col-md-12">
-                <img id="Untitled-1" src="<?= base_url('assets/images/Untitled-1.jpeg') ?>" srcset="Untitled-1.jpeg 1x, Untitled-1@2x.jpeg 2x">
-                <img id="Untitled-1_u" src="<?= base_url('assets/images/Untitled-1_u.jpeg') ?>" srcset="Untitled-1_u.jpeg 1x, Untitled-1_u@2x.jpeg 2x">
+                <img id="Untitled-1" src="<?= base_url('assets/images/Untitled-1.jpeg') ?>" srcset="<?= base_url('assets/images/Untitled-1.jpeg') ?> 1x, <?= base_url('assets/images/Untitled-1@2x.jpeg') ?> 2x">
+                <img id="Untitled-1_u" src="<?= base_url('assets/images/Untitled-1_u.jpeg') ?>" srcset="<?= base_url('assets/images/Untitled-1_u.jpeg') ?> 1x, <?= base_url('assets/images/Untitled-1_u@2x.jpeg') ?> 2x">
 
                 <div id="SAR_200_v">
                     <span>SAR</span><span style="font-size:45px;"> </span><span style="font-size:50px;">200</span>
