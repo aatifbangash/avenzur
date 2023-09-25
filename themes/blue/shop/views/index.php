18da05df341eb30cf3cb16eaba5bea6d8d4e8be7
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-25 10:59:45 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -60,6 +60,16 @@
     <div class="container">
         <div class="row" style="padding:40px 40px !important;">
             <div class="col-md-12">
+                <img src="banner.jpg" />
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="page-contents" style="background:#FFFFFF !important;">
+    <div class="container">
+        <div class="row" style="padding:40px 40px !important;">
+            <div class="col-md-12">
                 <!--<img src="<?php //echo base_url('assets/images/SD-Italy.svg') ?>" style="margin: 0 auto; display: block;" class="img-responsive icon" > -->
                 <h2 style="text-align:center !important;   " class="select-head"> Featured Categories</h2>
                         <?php
