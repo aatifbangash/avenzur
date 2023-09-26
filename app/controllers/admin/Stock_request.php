886803050f8d4f34cd97501a7f2caacf09da0b1d
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-26 15:01:45 +0300

    'c'

--- app/controllers/admin/Stock_request.php
+++ app/controllers/admin/Stock_request.php
@@ -333,6 +333,7 @@ class stock_request extends MY_Controller
             }
         }else{
             $warehouse_id = isset($_POST['warehouse']) ? $_POST['warehouse'] : $_POST['warehouse_id'];
+            echo 'WarehouseId: '.$warehouse_id;
             $current_pr = $this->stock_request_model->getCurrentPR($warehouse_id);
             $this->data['current_pr'] = $current_pr;
             $this->data['warehouse_id'] = $warehouse_id;
