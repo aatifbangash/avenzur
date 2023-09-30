1317cb729b992765bbd36fb06de2f40cd9fe0f4c
Faisal Abbas <faisalabbas67@gmail.com>
2023-09-30 17:30:16 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -72,7 +72,7 @@
                                         foreach ($fps as $fp) {
                                             ?>
                                         <div class="col-sm-2" id="select-prod" style="padding: 0px !important;">
-                                                <div class="product alt " style="border-radius: 0px !important;border: 0px !important;padding: 0px !important;">
+                                                <div class="product alt " style="border-radius: 0px !important;border: 0px !important;padding: 25px !important;">
         
                                                     <div class="product-top">
                                                         <div class="image">
