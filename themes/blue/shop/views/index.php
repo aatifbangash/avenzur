5f53c40c64b776842cc36f670506618dcc266c23
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-02 13:22:20 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -272,8 +272,8 @@
                                             </div>
                                         </div>
                                         <div class="product-desc homeprod" style="height: 52%;">
-                                            <div class="product_name" style="overflow: inherit;display: inherit;">
-                                                <a href="<?= site_url('product/' . $fp->slug); ?>"><?= $fp->name; ?></a>
+                                            <div class="product_name" style="overflow: inherit;display: inherit;font-size: 16px;">
+                                                <a style="cursor: pointer;" href="<?= site_url('product/' . $fp->slug); ?>"><?= $fp->name; ?></a>
                                             </div>
                                             <div class="pro-cat" style="font-size: 16px;">
                                                 <a href="<?= site_url('category/' . $fp->category_slug); ?>" class="link"><?= $fp->category_name; ?></a>
