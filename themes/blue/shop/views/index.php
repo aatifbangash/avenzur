4d104dce59fe500fb8bdaec15943d1963177bb27
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-02 10:36:15 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -201,48 +201,46 @@
     <div class="container">
          <div class="row" style="padding:40px 15px !important;">
              <div class="col-md-12">
-             <div id="carousel1" class='outerWrapper'>
-             <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
+                <div id="carousel1" class='outerWrapper'>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
+                    </div>
+                    <div class="carousel-item">
+                        <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/9b0ec372041c805dada856cd0ba83438.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/2e2b4769f47726f01ee1ec487b5d5206.jpeg') ?>"></div>
-                </div>
-                <div class="carousel-item">
-                    <div><img width="223" height="240" src="<?= base_url('assets/images/a67dd40afc0aeef53a1222cb80c01b27.jpeg') ?>"></div>
-                </div>
+                <script>$('#carousel1').waltzer({scroll:1});</script>
             </div>
-
-            <script>$('#carousel1').waltzer({scroll:1});</script>
-             </div>
-
             
             <div class="col-md-12">
                 <div class="banner1" style="display: flex;justify-content: center;align-items: center;margin-bottom: 15px;">
@@ -258,99 +256,78 @@
                 </div>
             </div>
         
-             <div class="col-md-12" style="margin-top: 25px;margin-bottom: 30px;">
-                        <h2 style="text-align:center !important;" class="select-head"> Featured Products</h2>
-                        <?php
-                        $r = 0;
-                        foreach ($featured_products as $fp) {
-                            //echo '<pre>';print_r($fps);
-                            ?>
-                            <div class="item row active" style="margin-top: 40px;">
-                                <div class="selected-products">
-                                        <?php
-                                        //foreach ($fps as $fp) {
-                                            ?>
-                                        <div class="col-sm-6 col-md-3">
-                                                <div class="product alt ">
-                                                    <div class="product-top">
-                                                        <div class="image">
-                                                            <a href="<?= site_url('product/' . $fp->slug); ?>">
-                                                                <img src="<?= base_url('assets/uploads/' . $fp->image); ?>" alt="" class="img-responsive">
-                                                            </a>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-desc homeprod">
-                                                        <div class="product_name">
-                                                            <a href="<?= site_url('product/' . $fp->slug); ?>"><?= $fp->name; ?></a>
-                                                        </div>
-                                                        <div class="pro-cat">
-                                                            <a href="<?= site_url('category/' . $fp->category_slug); ?>" class="link"><?= $fp->category_name; ?></a>
-                                                            <?php
-                                                            if ($fp->brand_name) {
-                                                                ?>
-                                                                <span class="link">-</span>
-                                                                <a href="<?= site_url('brand/' . $fp->brand_slug); ?>" class="link"><?= $fp->brand_name; ?></a>
-                                                                <?php
-                                                            } ?>
-                                                        </div>
-                                                    </div>
-                                                    <div class="product-bottom">
-                                                        <div class="product-price">
-                                                        <?php
-                                                            if ($fp->promotion) {
-                                                                echo '<del class="text-red">' . $this->sma->convertMoney(isset($fp->special_price) && !empty(isset($fp->special_price)) ? $fp->special_price : $fp->price) . '</del><br>';
-                                                                echo $this->sma->convertMoney($fp->promo_price);
-                                                            } else {
-                                                                echo $this->sma->convertMoney(isset($fp->special_price) && !empty(isset($fp->special_price)) ? $fp->special_price : $fp->price);
-                                                            } ?>
-                                                        </div>
-                                          
-                                                        <!--<div class="details" style="transition: all 100ms ease-out 0s;">-->
-
-                                                        <div class="clearfix"></div>
-                                                
-                                                        <div class="row">
-                                                    
-                                                        <div class="col-md-7 col-xs-6">
-                                                            <div class="product-quantity">
-                                                                <div class="form-group" style="margin-bottom:0;">
-                                                                    <div class="input-group">
-                                                                        <span class="input-group-addon pointer btn-minus"><span class="fa fa-minus"></span></span>
-                                                                        <input type="text" name="quantity" class="form-control text-center quantity-input prod_quant" value="1" required="required">
-                                                                        <span class="input-group-addon pointer btn-plus"><span class="fa fa-plus"></span></span>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-
-                                                        <div class="col-md-5 col-xs-6">
-                                                            <div class="clearfix"></div>
-                                                                <div class="product-cart">
-                                                                    <div class="btn-group" role="group" aria-label="...">
-                                                                        <button class="btn btn-info add-to-wishlist" data-id="13"><i class="fa fa-heart-o"></i></button>
-                                                                        <div class="btn btn-theme add-to-cart" data-id="<?= $fp->id; ?>"><i class="fa fa-shopping-cart"></i> <?= lang('add_to_cart'); ?>
-                                                                    </div>
-                                                                <div class="clearfix"></div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>  
-                                            </div>                
-                                        </div>
-                                </div>
-                                        <?php
-                                    //} ?>
-                            </div>
-                            </div>
+            <div class="col-md-12" style="margin-top: 25px;margin-bottom: 30px;">
+                <h2 style="text-align:center !important;" class="select-head"> Featured Products</h2>
+                <?php
+                    $r = 0;
+                    foreach ($featured_products as $fp) { ?>
                         
-                         
-                          
-                            <?php
-                            $r++;
-                            //break;
-                        }
-                        ?>
-                 </div>
+                                    <div class="product alt ">
+                                        <div class="product-top">
+                                            <div class="image">
+                                                <a href="<?= site_url('product/' . $fp->slug); ?>">
+                                                    <img src="<?= base_url('assets/uploads/' . $fp->image); ?>" alt="" class="img-responsive">
+                                                </a>
+                                            </div>
+                                        </div>
+                                        <div class="product-desc homeprod">
+                                            <div class="product_name">
+                                                <a href="<?= site_url('product/' . $fp->slug); ?>"><?= $fp->name; ?></a>
+                                            </div>
+                                            <div class="pro-cat">
+                                                <a href="<?= site_url('category/' . $fp->category_slug); ?>" class="link"><?= $fp->category_name; ?></a>
+                                                <?php
+                                                    if ($fp->brand_name) {
+                                                        ?>
+                                                        <span class="link">-</span>
+                                                        <a href="<?= site_url('brand/' . $fp->brand_slug); ?>" class="link"><?= $fp->brand_name; ?></a>
+                                                        <?php
+                                                    } ?>
+                                            </div>
+                                        </div>
+                                        <div class="product-bottom">
+                                            <div class="product-price">
+                                                <?php
+                                                    if ($fp->promotion) {
+                                                        echo '<del class="text-red">' . $this->sma->convertMoney(isset($fp->special_price) && !empty(isset($fp->special_price)) ? $fp->special_price : $fp->price) . '</del><br>';
+                                                        echo $this->sma->convertMoney($fp->promo_price);
+                                                    } else {
+                                                        echo $this->sma->convertMoney(isset($fp->special_price) && !empty(isset($fp->special_price)) ? $fp->special_price : $fp->price);
+                                                    } 
+                                                ?>
+                                            </div>
+                                            <div class="clearfix"></div>
+                                            <div class="row">
+                                                <div class="col-md-7 col-xs-6">
+                                                    <div class="product-quantity">
+                                                        <div class="form-group" style="margin-bottom:0;">
+                                                            <div class="input-group">
+                                                                <span class="input-group-addon pointer btn-minus"><span class="fa fa-minus"></span></span>
+                                                                <input type="text" name="quantity" class="form-control text-center quantity-input prod_quant" value="1" required="required">
+                                                                <span class="input-group-addon pointer btn-plus"><span class="fa fa-plus"></span></span>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-5 col-xs-6">
+                                                    <div class="clearfix"></div>
+                                                        <div class="product-cart">
+                                                            <div class="btn-group" role="group" aria-label="...">
+                                                                <button class="btn btn-info add-to-wishlist" data-id="13"><i class="fa fa-heart-o"></i></button>
+                                                                <div class="btn btn-theme add-to-cart" data-id="<?= $fp->id; ?>"><i class="fa fa-shopping-cart"></i> <?= lang('add_to_cart'); ?>
+                                                            </div>
+                                                            <div class="clearfix"></div>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>  
+                                        </div> 
+                                    </div>
+                                
+                    <?php
+                    }
+                    ?>
+                </div>
                     
                 <div class="col-md-12">
                     <div class="banner11" style="display: flex;justify-content: center;align-items: center;margin-bottom: 15px;">
