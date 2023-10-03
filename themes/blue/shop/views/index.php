f6129115eff07226911844ed8f50844bacdbb3bf
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-03 10:25:14 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -303,18 +303,15 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                </div>
-                                                <div class="col-md-5 col-xs-6">
-                                                    <div class="clearfix"></div>
-                                                        <div class="product-cart">
-                                                            <div class="btn-group" role="group" aria-label="...">
-                                                                <button class="btn btn-info add-to-wishlist" data-id="13"><i class="fa fa-heart-o"></i></button>
-                                                                <div class="btn btn-theme add-to-cart" data-id="<?= $fp->id; ?>"><i class="fa fa-shopping-cart"></i> <?= lang('add_to_cart'); ?>
-                                                            </div>
-                                                            <div class="clearfix"></div>
+                                                    <div class="product-cart">
+                                                        <div class="btn-group" role="group" aria-label="...">
+                                                            <button class="btn btn-info add-to-wishlist" data-id="13"><i class="fa fa-heart-o"></i></button>
+                                                            <div class="btn btn-theme add-to-cart" data-id="<?= $fp->id; ?>"><i class="fa fa-shopping-cart"></i> <?= lang('add_to_cart'); ?>
                                                         </div>
+                                                        <div class="clearfix"></div>
                                                     </div>
                                                 </div>
+                                                
                                             </div>  
                                         </div> 
                                     </div>
