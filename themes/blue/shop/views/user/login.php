4258aa06400722b40b5f9b5215f4d527ac45c623
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-08 15:23:32 +0300

    'c'

--- themes/blue/shop/views/user/login.php
+++ themes/blue/shop/views/user/login.php
@@ -91,7 +91,7 @@
                                         <div class="form-group">
                                             <?= lang('phone ', 'phone '); ?>
                                             <div class="controls">
-                                                  <input type="tel" id="phone" name="phone" class="form-control" data-country="sa" />
+                                                  <input type="tel" id="phone" name="phone" class="form-control" />
                                                 <!--<?= form_input('phone', '', 'class="form-control" id="phone" '); ?>-->
                                             </div>
                                         </div>
@@ -241,7 +241,7 @@
     // Vanilla Javascript
     var input = document.querySelector("#phone");
     window.intlTelInput(input,({
-      // options here
+        initialCountry: "In"
     }));
 function LoginFn(){
              
