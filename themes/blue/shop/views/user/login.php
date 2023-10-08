5a14013c34b1730b616812f892ac42c93efc8ed5
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-08 11:44:12 +0300

    'c'

--- themes/blue/shop/views/user/login.php
+++ themes/blue/shop/views/user/login.php
@@ -70,20 +70,18 @@
                                     <div class="col-sm-6">
                                         <div class="form-group">
                                         <?= lang('Country', 'Country'); ?>
-                                        <!--<select class="form-control" id="country" name="country" >-->
+                                        <select class="form-control" id="country" name="country" >
                                        
-                                         <!--<option value="AE">AE</option>-->
-                                       <!--<option value="">Select Country</option>-->
+                                        <option value="AE">AE</option>
+                                        <option value="">Select Country</option>
                                         <?php
                                             foreach($country as $country)
                                             {
-                                                echo $country->code.' ---- '.$country_code;
                                                 $selected = (trim($country->code) == trim($country_code)) ? 'selected' : '';
-                                                echo 'Selected is: '.$selected;
-                                                //echo '<option value="'.$country->code.'"' . $selected . '>'.$country->name.'</option>';
+                                                echo '<option value="'.$country->code.'"' . $selected . '>'.$country->name.'</option>';
                                             }
                                         ?>                    
-                                        <!--</select>-->
+                                        </select>
                             
 
                                         </div>
