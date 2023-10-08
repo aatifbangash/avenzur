74afc8e4fab4478174476df224ee56492f0f1d50
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-08 10:43:42 +0300

    'c'

--- themes/blue/admin/views/auth/login.php
+++ themes/blue/admin/views/auth/login.php
@@ -236,7 +236,7 @@ foreach ($bgs as &$bg) {
             <?php
             if ($Settings->allow_reg) {
                 ?>
-                <div id="register">
+                <div id="register">Peeeengaaa
                     <div class="container">
                         <div class="registration-form-div reg-content">
                             <?php echo admin_form_open('auth/register', 'class="login" data-toggle="validator"'); ?>
