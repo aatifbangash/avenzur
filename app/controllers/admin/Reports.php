550b368c52f7c4aff98eead06e6798ab260aaf95
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-22 15:16:56 +0300

    'c'

--- app/controllers/admin/Reports.php
+++ app/controllers/admin/Reports.php
@@ -3863,10 +3863,9 @@ class Reports extends MY_Controller
 
             $inventryReportData = [];
             $productOpeningsData = $this->reports_model->getProductsQuantityUnitCost($start_date,$from_warehouse_id);
-            print_r($productOpeningsData);exit;
 
             $productInOutData = $this->reports_model->getInventoryTrialBalanceData($start_date, $end_date, $from_warehouse_id, $to_warehouse_id);
-            
+            print_r($productInOutData);exit;
             foreach($productInOutData as $prdId => $row){
 
                 $productCost = 1;
