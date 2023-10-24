b56be74849d5041eee8e58a0fbf00489deaba489
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-24 12:40:30 +0300

    'c'

--- themes/blue/shop/views/footer.php
+++ themes/blue/shop/views/footer.php
@@ -75,5 +75,62 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script  src="<?= $assets; ?>js/ecommerce-main.js"></script>
     <script  src="<?= $assets; ?>js/script.min.js"></script>
+    <script type="text/javascript">
+    var m = '<?= $m; ?>', v = '<?= $v; ?>', products = {}, filters = <?= isset($filters) && !empty($filters) ? json_encode($filters) : '{}'; ?>, shop_color, shop_grid, sorting;
+
+    var cart = <?= isset($cart) && !empty($cart) ? json_encode($cart) : '{}' ?>;
+    var site = {base_url: '<?= base_url(); ?>', site_url: '<?= site_url('/'); ?>', shop_url: '<?= shop_url(); ?>', csrf_token: '<?= $this->security->get_csrf_token_name() ?>', csrf_token_value: '<?= $this->security->get_csrf_hash() ?>', settings: {display_symbol: '<?= $Settings->display_symbol; ?>', symbol: '<?= $Settings->symbol; ?>', decimals: <?= $Settings->decimals; ?>, thousands_sep: '<?= $Settings->thousands_sep; ?>', decimals_sep: '<?= $Settings->decimals_sep; ?>', order_tax_rate: false, products_page: <?= $shop_settings->products_page ? 1 : 0; ?>}, shop_settings: {private: <?= $shop_settings->private ? 1 : 0; ?>, hide_price: <?= $shop_settings->hide_price ? 1 : 0; ?>}}
+
+    var lang = {};
+    lang.page_info = '<?= lang('page_info'); ?>';
+    lang.cart_empty = '<?= lang('empty_cart'); ?>';
+    lang.item = '<?= lang('item'); ?>';
+    lang.items = '<?= lang('items'); ?>';
+    lang.unique = '<?= lang('unique'); ?>';
+    lang.total_items = '<?= lang('total_items'); ?>';
+    lang.total_unique_items = '<?= lang('total_unique_items'); ?>';
+    lang.tax = '<?= lang('tax'); ?>';
+    lang.shipping = '<?= lang('shipping'); ?>';
+    lang.total_w_o_tax = '<?= lang('total_w_o_tax'); ?>';
+    lang.product_tax = '<?= lang('product_tax'); ?>';
+    lang.order_tax = '<?= lang('order_tax'); ?>';
+    lang.total = '<?= lang('total'); ?>';
+    lang.grand_total = '<?= lang('grand_total'); ?>';
+    lang.reset_pw = '<?= lang('forgot_password?'); ?>';
+    lang.type_email = '<?= lang('type_email_to_reset'); ?>';
+    lang.submit = '<?= lang('submit'); ?>';
+    lang.error = '<?= lang('error'); ?>';
+    lang.add_address = '<?= lang('add_address'); ?>';
+    lang.update_address = '<?= lang('update_address'); ?>';
+    lang.fill_form = '<?= lang('fill_form'); ?>';
+    lang.already_have_max_addresses = '<?= lang('already_have_max_addresses'); ?>';
+    lang.send_email_title = '<?= lang('send_email_title'); ?>';
+    lang.message_sent = '<?= lang('message_sent'); ?>';
+    lang.add_to_cart = '<?= lang('add_to_cart'); ?>';
+    lang.out_of_stock = '<?= lang('out_of_stock'); ?>';
+    lang.x_product = '<?= lang('x_product'); ?>';
+    lang.r_u_sure = '<?= lang('r_u_sure'); ?>';
+    lang.x_reverted_back = "<?= lang('x_reverted_back'); ?>";
+    lang.delete = '<?= lang('delete'); ?>';
+    lang.line_1 = '<?= lang('line1'); ?>';
+    lang.line_2 = '<?= lang('line2'); ?>';
+    lang.city = '<?= lang('city'); ?>';
+    lang.state = '<?= lang('state'); ?>';
+    lang.postal_code = '<?= lang('postal_code'); ?>';
+    lang.country = '<?= lang('country'); ?>';
+    lang.phone = '<?= lang('phone'); ?>';
+    lang.is_required = '<?= lang('is_required'); ?>';
+    lang.okay = '<?= lang('okay'); ?>';
+    lang.cancel = '<?= lang('cancel'); ?>';
+    lang.email_is_invalid = '<?= lang('email_is_invalid'); ?>';
+    lang.name = '<?= lang('name'); ?>';
+    lang.full_name = '<?= lang('full_name'); ?>';
+    lang.email = '<?= lang('email'); ?>';
+    lang.subject = '<?= lang('subject'); ?>';
+    lang.message = '<?= lang('message'); ?>';
+    lang.required_invalid = '<?= lang('required_invalid'); ?>';
+
+    update_mini_cart(cart);
+    </script>
   </body>
 </html>
