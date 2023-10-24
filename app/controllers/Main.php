346442e2d88eb74512617e6dfcda91a59ec7a47b
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-24 11:05:39 +0300

    'c'

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -130,9 +130,7 @@ class Main extends MY_Shop_Controller
         $this->data['all_categories']    = $this->shop_model->getAllCategories();
         $this->data['featured_categories'] = $this->shop_model->getFeaturedCategories();
         $this->data['popular_categories'] = $this->shop_model->getPopularCategories();
-        echo '<pre>';
-        print_r($this->data['popular_categories']);
-        exit;
+        
         $this->data['featured_products'] = $this->shop_model->getFeaturedProducts();
         $this->data['special_offers']    = $this->shop_model->getSpecialOffers();
         $this->data['slider']            = json_decode($this->shop_settings->slider);
