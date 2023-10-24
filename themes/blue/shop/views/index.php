27dd7781c08b217ed6c9cd7c7ee2d0fd2418f7e1
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-24 12:04:05 +0300

    'c'

--- themes/blue/shop/views/index.php
+++ themes/blue/shop/views/index.php
@@ -314,7 +314,7 @@
                                                 </div>
                                             <!--</a>-->
                                             <div> 
-                                                <button type="button" class="btn primary-buttonAV mt-3 py-1 addtocart w-100 text-dark">Add to cart </button>
+                                                <button type="button" class="btn primary-buttonAV mt-3 py-1 addtocart w-100 text-dark add-to-cart">Add to cart </button>
                                             </div>
                                         </div> 
                                     </div>
