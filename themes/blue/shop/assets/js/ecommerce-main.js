bc32c23f861b79c402ca033def2600a1178eeb2e
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-25 13:42:44 +0300

    'c'

--- themes/blue/shop/assets/js/ecommerce-main.js
+++ themes/blue/shop/assets/js/ecommerce-main.js
@@ -74,6 +74,42 @@ function get(t) {
   alert("Please use a modern browser as this site needs localstroage!")
 }
 
+function gen_html(t) {
+  var e = "";
+  if (get_width() > 992)
+      var a = get("shop_grid")
+        , s = ".three-col" == a ? 3 : 4;
+  else
+      var a = ".four-col"
+        , s = 4;
+  var i = a && ".three-col" == a ? "col-sm-6 col-md-4" : "col-md-6"
+    , o = a && ".three-col" == a ? "alt" : "";
+  if (t || (e += '<div class="col-sm-12"><div class="alert alert-warning text-center padding-xl margin-top-lg"><h4 class="margin-bottom-no">' + lang.x_product + "</h4></div></div>"),
+  1 == site.settings.products_page && ($("#results").empty(),
+  $(".grid").isotope("destroy").isotope()),
+  $.each(t, function(a, r) {
+      var n = r.special_price ? r.special_price : r.price
+        , l = r.special_price ? r.formated_special_price : r.formated_price
+        , c = (r.promotion && r.promo_price && 0 != r.promo_price && r.promo_price,
+      r.promotion && r.promo_price && 0 != r.promo_price ? r.formated_promo_price : l);
+      1 != site.settings.products_page && (0 === a ? e += '<div class="row">' : a % s == 0 && (e += '</div><div class="row">')),
+      e += '<div class="product-container ' + i + " " + (1 == site.settings.products_page ? "grid-item" : "") + '">\n        <div class="product ' + o + " " + (1 == site.settings.products_page ? "grid-sizer" : "") + '">\n        ' + (r.promo_price ? '<span class="badge badge-right theme">Promo</span>' : "") + '\n        <div class="product-top">\n        <div class="product-image">\n        <a href="' + site.site_url + "product/" + r.slug + '">\n        <img class="img-responsive" src="' + site.base_url + "assets/uploads/" + r.image + '" alt=""/>\n        </a>\n        </div>\n        <div class="product-desc">\n        <a href="' + site.site_url + "product/" + r.slug + '">\n        <h2 class="product-name">' + r.name + "</h2>\n        </a>\n        <p>" + r.details + '</p>\n        </div>\n        </div>\n        <div class="clearfix"></div>\n        ' + (1 == site.shop_settings.hide_price ? "" : '\n        <div class="product-bottom">\n        <div class="product-price">\n        ' + (r.promo_price ? '<del class="text-danger text-size-sm">' + l + "</del>" : "") + "\n        " + c + '\n        </div>\n        <div class="product-rating">\n        <div class="form-group" style="margin-bottom:0;">\n        <div class="input-group">\n        <span class="input-group-addon pointer btn-minus"><span class="fa fa-minus"></span></span>\n        <input type="text" name="quantity" class="form-control text-center quantity-input" value="1" required="required">\n        <span class="input-group-addon pointer btn-plus"><span class="fa fa-plus"></span></span>\n        </div>\n        </div>\n        </div>\n        <div class="clearfix"></div>\n        <div class="product-cart-button">\n        <div class="btn-group" role="group" aria-label="...">\n        <button class="btn btn-info add-to-wishlist" data-id="' + r.id + '"><i class="fa fa-heart-o"></i></button>\n        <button class="btn btn-theme add-to-cart" data-id="' + r.id + '"><i class="fa fa-shopping-cart padding-right-md"></i> ' + lang.add_to_cart + '</button>\n        </div>\n        </div>\n        <div class="clearfix"></div>\n        </div>') + '\n        </div>\n        <div class="clearfix"></div>\n        </div>',
+      1 != site.settings.products_page && a + 1 === t.length && (e += "</div>")
+  }),
+  1 != site.settings.products_page)
+      $("#results").empty(),
+      $(e).appendTo($("#results"));
+  else {
+      var r = $(e);
+      $(".grid").isotope("insert", r).isotope("layout"),
+      setTimeout(function() {
+          $(".grid").isotope({
+              itemSelector: ".grid-item"
+          })
+      }, 200)
+  }
+}
+
 function get_filters() {
   return filters.category = $("#product-category").val() ? $("#product-category").val() : filters.category,
   filters.min_price = $("#min-price").val(),
