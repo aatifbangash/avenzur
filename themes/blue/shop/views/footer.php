e9efa5d8df796098172e50373c864b900be9be62
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-26 12:41:57 +0300

    'c'

--- themes/blue/shop/views/footer.php
+++ themes/blue/shop/views/footer.php
@@ -73,9 +73,9 @@
     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script  src="<?= $assets; ?>js/ecommerce-main.js"></script>
-    <script  src="<?= $assets; ?>js/script.min.js"></script>
     <script type="text/javascript">
     var m = '<?= $m; ?>', v = '<?= $v; ?>', products = {}, filters = <?= isset($filters) && !empty($filters) ? json_encode($filters) : '{}'; ?>, shop_color, shop_grid, sorting;
 
