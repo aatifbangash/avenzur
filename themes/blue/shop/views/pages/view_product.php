be2d8c338301451545278132847b70b4a1c9a46e
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-26 13:11:31 +0300

    'c'

--- themes/blue/shop/views/pages/view_product.php
+++ themes/blue/shop/views/pages/view_product.php
@@ -114,9 +114,9 @@
         </ul>
         <div class="tab-content" id="productTabsContent">
             <div class="tab-pane fade show active" id="description" role="tabpanel" aria-labelledby="description-tab">
-            <p class="m-0 py-3 lh-base fs-6">But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.
-
-                To take a trivial example, which of us ever undertakes laborious physical exercise, except to obtain some advantage from it? But who has any right to find fault with a man who chooses to enjoy a pleasure that has no annoying consequences, or one who avoids a pain that produces no resultant pleasure? On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire.</p>
+            <p class="m-0 py-3 lh-base fs-6">
+                <?= $product->details; ?>
+            </p>
             </div>
             <div class="tab-pane fade" id="additional" role="tabpanel" aria-labelledby="additional-tab">
             
