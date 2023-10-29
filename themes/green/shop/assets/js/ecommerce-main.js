fa9d773de23fd34c3bc618931dbea16f7a4ed921
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-29 11:18:08 +0300

    'c'

--- themes/green/shop/assets/js/ecommerce-main.js
+++ themes/green/shop/assets/js/ecommerce-main.js
@@ -85,6 +85,7 @@ function update_cart_item(t, e, a, s, i) {
 }
 
 function update_cart(t) {
+  console.log('Here we are in update cart....');
   if (t.total_items && t.total_items > 0) {
       $("#cart-table tbody").empty();
       var e = 1;
