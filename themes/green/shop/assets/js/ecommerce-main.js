2dc27c0dce323f40c60dc60b91a76b4b661bc831
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-29 12:54:00 +0300

    'c'

--- themes/green/shop/assets/js/ecommerce-main.js
+++ themes/green/shop/assets/js/ecommerce-main.js
@@ -50,16 +50,15 @@ function update_mini_cart(t) {
     $(".cart-total-items").text(t.total_items);
     //$(".cart-total-items").text(t.total_items + " " + (t.total_items > 1 ? lang.items : lang.item));
     //$("#cart-items-table").empty(),
-      $.each(t.contents, function() {
-          var t = '<td><a href="' + site.site_url + "/product/" + this.slug + '"><span class="cart-item-image"><img src="' + site.base_url + "assets/uploads/thumbs/" + this.image + '" alt=""></span></a></td><td><a href="' + site.site_url + "/product/" + this.slug + '">' + this.name + "</a><br>" + this.qty + " x " + this.price + '</td><td class="text-right text-bold">' + this.subtotal + "</td>";
-          //$("<tr>" + t + "</tr>").appendTo("#cart-body")
-          $("#cart-body").append("<tr>" + t + "</tr>");
-      });
-      var e = '\n        <tr><td colspan="2">' + lang.total_items + '</td><td class="text-end fw-bold">' + t.total_items + '</td></tr>\n        <tr><td>' + lang.total + '</td><td colspan="2" class="text-end fw-bold">' + t.total + "</td></tr>\n        ";
-      console.log(e);
-      $("#cart-foot").html(e);
-      $("#cart-empty").hide();
-      //$("#cart-contents").show()
+    /*$.each(t.contents, function() {
+        var t = '<td><a href="' + site.site_url + "/product/" + this.slug + '"><span class="cart-item-image"><img src="' + site.base_url + "assets/uploads/thumbs/" + this.image + '" alt=""></span></a></td><td><a href="' + site.site_url + "/product/" + this.slug + '">' + this.name + "</a><br>" + this.qty + " x " + this.price + '</td><td class="text-right text-bold">' + this.subtotal + "</td>";
+        //$("<tr>" + t + "</tr>").appendTo("#cart-body")
+        $("#cart-body").append("<tr>" + t + "</tr>");
+    });
+    var e = '\n  <tr><td colspan="2">' + lang.total_items + '</td><td class="text-end fw-bold">' + t.total_items + '</td></tr>\n        <tr><td>' + lang.total + '</td><td colspan="2" class="text-end fw-bold">' + t.total + "</td></tr>\n        ";
+    $("#cart-foot").html(e);*/
+    //$("#cart-empty").hide();
+    //$("#cart-contents").show()
   } else{
     $("#cart-items-table").empty();
     //$(".cart-total-items").text(lang.cart_empty);
