e3d76c58737d92dd01bc02246ce4b5aed1a9dda1
Faisal Abbas <faisalabbas67@gmail.com>
2023-10-29 15:17:09 +0300

    'c'

--- themes/green/shop/views/header.php
+++ themes/green/shop/views/header.php
@@ -11,6 +11,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
     <link href="<?= $assets; ?>css/libs.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     <link href="<?= $assets; ?>css/ecommerce-main.css" rel="stylesheet">
     <link rel="stylesheet" href="<?= $assets; ?>build/css/intlTelInput.css">
     <script src="<?= $assets; ?>build/js/intlTelInput.js"></script>
