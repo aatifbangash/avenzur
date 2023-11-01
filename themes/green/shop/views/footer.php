0d005b8c6f4ff59ff2e215eba8e73b8f6b374345
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-01 11:54:25 +0300

    'c'

--- themes/green/shop/views/footer.php
+++ themes/green/shop/views/footer.php
@@ -75,6 +75,7 @@
     <!--<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>-->
     <script src="<?= $assets; ?>js/libs.min.js"></script>
+    <script src="<?= $assets; ?>js/jquery.toast.min.js"></script>
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
     <script  src="<?= $assets; ?>js/ecommerce-main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
