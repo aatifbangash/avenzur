54f82c86589431479c5d0774a30d9f771a97270f
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-02 11:04:55 +0300

    'c'

--- app/controllers/Pay.php
+++ app/controllers/Pay.php
@@ -846,13 +846,12 @@ class Pay extends MY_Shop_Controller
             $this->data['return_rows']   = $order->return_id ? $this->shop_model->getOrderItems($order->return_id) : null;
             $this->data['Settings']      = $this->Settings;
             $this->data['shop_settings'] = $this->shop_settings;
-            //$html                        = $this->load->view($this->Settings->theme . '/shop/views/pages/pdf_invoice', $this->data, true);
-            /*if ($this->input->get('view')) {
+            $html                        = $this->load->view($this->Settings->theme . '/shop/views/pages/pdf_invoice', $this->data, true);
+            if ($this->input->get('view')) {
                 echo $html;
                 exit;
-            }*/
+            }
             $name = lang('invoice') . '_' . str_replace('/', '_', $order->reference_no) . '.pdf';
-            echo $name;exit;
             if ($buffer_save) {
                 return $this->sma->generate_pdf($html, $name, $buffer_save, $this->data['biller']->invoice_footer);
             }
