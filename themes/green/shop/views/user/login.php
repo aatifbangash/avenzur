66e102378c66d33be1bcecc1b1a3e060ab10c194
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-02 15:22:54 +0300

    'c'

--- themes/green/shop/views/user/login.php
+++ themes/green/shop/views/user/login.php
@@ -170,7 +170,7 @@
         $("#register").show();  
     }
 
-    /*var input = document.querySelector("#phone");
+    var input = document.querySelector("#phone");
     window.intlTelInput(input,({
         initialCountry: "<?= trim($country_code); ?>"
     }));
@@ -184,7 +184,7 @@
        });
  
          
-    });*/
+    });
          
 
   </script>
