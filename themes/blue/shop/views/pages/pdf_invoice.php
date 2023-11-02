f0ccc5d591d67d92fb6128b6e1c06119ff5e8c50
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-02 11:16:53 +0300

    'c'

--- themes/blue/shop/views/pages/pdf_invoice.php
+++ themes/blue/shop/views/pages/pdf_invoice.php
@@ -58,8 +58,8 @@
                     <?php
                     $qrtext = $this->inv_qrcode->base64([
                         'seller' => $biller->company && $biller->company != '-' ? $biller->company : $biller->name,
-                        'vat_no' => $biller->vat_no ?: $biller->get_no,
-                        'date' => $inv->date,
+                        //'vat_no' => $biller->vat_no ?: $biller->get_no,
+                        //'date' => $inv->date,
                         //'grand_total' => $return_sale ? ($inv->grand_total + $return_sale->grand_total) : $inv->grand_total,
                         //'total_tax_amount' => $return_sale ? ($inv->total_tax + $return_sale->total_tax) : $inv->total_tax,
                     ]);
