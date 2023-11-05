f07a5dc34c768306167ea4e056f6e0ab9e6184ba
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-05 15:42:30 +0300

    'c'

--- themes/green/shop/assets/js/ecommerce-main.js
+++ themes/green/shop/assets/js/ecommerce-main.js
@@ -521,7 +521,7 @@ $(document).ready(function(){
       /*,i=$(this).parents(".product").find("img").eq(0);if(i){i.clone().offset({top:i.offset().top,left:i.offset().left}).css({opacity:"0.5",position:"absolute",height:"150px",width:"150px","z-index":"1000"}).appendTo($("body")).animate({top:a.offset().top+10,left:a.offset().left+10,width:"50px",height:"50px"},400).animate({width:0,height:0},function(){$(this).detach()})}*/
       $.ajax({
           url: site.site_url + "cart/subscribe_newsletter",
-          type: "POST",
+          type: "GET",
           dataType: "json",
           data: {
             newsletterEmail: newsletterEmail
