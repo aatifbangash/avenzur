3933354c4a7a0990d281b295ae9f34b862ce524d
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-08 10:47:53 +0300

    'c'

--- themes/green/admin/views/products/index.php
+++ themes/green/admin/views/products/index.php
@@ -81,8 +81,6 @@
             {column_number: <?php $col++;
             echo $col; ?>, filter_default_label: "[<?=lang('quantity');?>]", filter_type: "text", data: []},
             {column_number: <?php $col++;
-            echo $col; ?>, filter_default_label: "[<?='Hide';?>]", filter_type: "text", data: []},
-            {column_number: <?php $col++;
             echo $col; ?>, filter_default_label: "[<?=lang('unit');?>]", filter_type: "text", data: []},
             <?php $col++; if ($warehouse_id && $Settings->racks) {
                 echo '{column_number : ' . $col . ', filter_default_label: "[' . lang('rack') . ']", filter_type: "text", data: [] },';
@@ -203,6 +201,7 @@
                             }
                             ?>
                             <th><?= lang('quantity') ?></th>
+                            <th><?= lang('hide') ?></th>
                             <th><?= lang('unit') ?></th>
                             <th><?= lang('rack') ?></th>
                             <th><?= lang('alert_quantity') ?></th>
