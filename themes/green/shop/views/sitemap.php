05c7374f3da4a9ec7a1e4ac847cf1068d42891ed
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-09 15:00:58 +0300

    'c'

--- themes/green/shop/views/sitemap.php
+++ themes/green/shop/views/sitemap.php
@@ -1,22 +1,18 @@
 <?php echo'<?xml version="1.0" encoding="UTF-8" ?>' ?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
-    <url>
-        <loc><?php echo base_url();?></loc>
-        <priority>1.0</priority>
-        <lastmod><?php echo date('Y-m-d'); ?></lastmod>
-        <changefreq>daily</changefreq>
-    </url>
+<?php echo'<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">'; ?>
+<?php echo'<url>'; ?>
+        <?php echo'<loc>'.base_url().'</loc>'; ?>
+        <?php echo'<priority>1.0</priority>'; ?>
+        <?php echo'<lastmod>'.date('Y-m-d').'</lastmod>'; ?>
+        <?php echo'<changefreq>daily</changefreq>'; ?>
+<?php echo'</url>'; ?>
  
- 
-    <!-- Sitemap -->
     <?php foreach($products as $product) { ?>
-    <url>
-        <loc><?php echo base_url()."product/".$product->slug ?></loc>
-        <priority>0.9</priority>
-        <lastmod><?php echo date('Y-m-d'); ?></lastmod>
-        <changefreq>daily</changefreq>
-    </url>
+      <?php echo'<url>'; ?>
+      <?php echo'<loc>'.base_url()."product/".$product->slug.'</loc>'; ?>
+      <?php echo'<priority>0.9</priority>'; ?>
+      <?php echo'<lastmod>'.date('Y-m-d').'</lastmod>'; ?>
+      <?php echo'<changefreq>daily</changefreq>'; ?>
+      <?php echo'</url>'; ?>
     <?php } ?>
- 
- 
-</urlset>
+<?php echo '</urlset>'; ?>
