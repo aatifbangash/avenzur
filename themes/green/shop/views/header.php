13b083375f05fb3d1b6b311994c14e5be4b61a1c
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-14 14:20:50 +0300

    'c'

--- themes/green/shop/views/header.php
+++ themes/green/shop/views/header.php
@@ -10,6 +10,9 @@
     <!--<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;1,100;1,300;1,400;1,700&family=Manrope:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link href="<?= $assets; ?>css/ecommerce-main.css" rel="stylesheet">
+
+    <script src="<?= $assets; ?>build/js/intlTelInput.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </head>
   
   <body>
