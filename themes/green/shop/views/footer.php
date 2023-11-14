54d4ff712553a393621b648d65a67a220ccf4b86
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-14 10:27:36 +0300

    'c'

--- themes/green/shop/views/footer.php
+++ themes/green/shop/views/footer.php
@@ -81,6 +81,8 @@
       
   </section>
     <!-- footer end -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="<?= $assets; ?>css/ecommerce-main.css" rel="stylesheet">
     <link href="<?= $assets; ?>css/fontfamily.css" rel="stylesheet">
     <!--<link href="<?php //echo $assets; ?>css/bootstrap.min.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css">
