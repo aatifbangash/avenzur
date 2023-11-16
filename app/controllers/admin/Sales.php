6dff344bde1f599fa239f38b31fee26592b4d293
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-16 17:15:58 +0300

    'c'

--- app/controllers/admin/Sales.php
+++ app/controllers/admin/Sales.php
@@ -2020,7 +2020,7 @@ class Sales extends MY_Controller
 
         $cipher_text = strtoupper(md5('Aa123456jadada236t2'));
         echo 'Cipher: '.$cipher_text.'<br />';
-        $digest = base64_encode(md5('J008624173'.$cipher_text.'a0a1047cce70493c9d5d29704f05d0d9'));
+        $digest = base64_encode(strtoupper(md5('J008624173'.$cipher_text.'a0a1047cce70493c9d5d29704f05d0d9')));
         echo 'Digest: '.$digest;exit;
         $address_id = $sale->address_id;
         $customer = $this->site->getCompanyByID($sale->customer_id);
