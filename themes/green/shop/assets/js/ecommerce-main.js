f63113c784bd6a1ef4df7aee3da13c9d1d3cc2c0
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-21 10:03:07 +0300

    'c'

--- themes/green/shop/assets/js/ecommerce-main.js
+++ themes/green/shop/assets/js/ecommerce-main.js
@@ -760,7 +760,7 @@ $(document).ready(function () {
     var e = $(this).attr("data-id"),
       a = $(".shopping-cart:visible"),
       s = $(this).parents(".card").find("input");
-      
+      console.log(s.val());
       if(!s){
         s = $(this).parents(".get-quantity").find("input");
       }
