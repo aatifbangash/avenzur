0121688089d9efc5d08ef1c9dea4239ff647f236
kamaljameel <kamaljameel14@gmail.com>
2023-11-22 22:51:06 +0500

    arabic language css fixes

--- themes/green/shop/views/pages/view_product.php
+++ themes/green/shop/views/pages/view_product.php
@@ -16,7 +16,7 @@
         
         <div class="row">
         <!-- product image col and thumb slider -->
-            <div class="col-md-5 col-12 pe-md-5">
+            <div class="col-md-5 col-12 pe-md-5 view_product_ar">
                 <div id="slider" class="owl-carousel product-slider">
                     <?php
                         if(isset($product->image)){
