c5280c1302e3a67760b49896e9d96c19df564d29
Faisal Abbas <faisalabbas67@gmail.com>
2023-11-22 10:21:23 +0300

    'c'

--- app/controllers/admin/Sales.php
+++ app/controllers/admin/Sales.php
@@ -1943,8 +1943,6 @@ class Sales extends MY_Controller
         $post_content = array(
             'bizContent' => $post_data
         );
-
-        print_r($post_content);exit;
     
         $postdata = http_build_query($post_content);
     
