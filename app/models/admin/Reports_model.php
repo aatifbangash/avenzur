021697651da0164cc0da26386f83db771df68a8c
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-03 16:04:42 +0300

    'c'

--- app/models/admin/Reports_model.php
+++ app/models/admin/Reports_model.php
@@ -993,7 +993,7 @@ class Reports_model extends CI_Model
             $totalPurchasesQuery .= "AND (p.id = '{$item}') ";
         }
 
-        $totalPurchasesQuery .= "GROUP BY p.code, p.name, pi.batchno, pi.warehouse_id
+        $totalPurchasesQuery .= "GROUP BY p.code, p.name, pi.warehouse_id
                                 ORDER BY p.id ASC, w.id ASC";
 
         $totalPurchseResultSet = $this->db->query($totalPurchasesQuery);
