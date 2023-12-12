3d57cfdc6b6c49c1a439c310f2730b315d283718
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-12 16:08:58 +0300

    'c'

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -393,9 +393,9 @@ class Main extends MY_Shop_Controller
             $message = 'Your One Time Password for Avenzur.com is '.$otp;
 
             if($medium == 'email'){
-                $this->sma->send_sms($identifier, $opt);
-            }else{
                 $this->sma->send_email($email, 'OTP Avenzur.com', $message, null, null, $attachment, [], []);
+            }else{
+                $this->sma->send_sms($identifier, $opt);
             }
 
             return true;
