d563ae3ef67db502a55c4e61fc82e6559e5e7e48
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-13 10:09:12 +0300

    'c'

--- themes/green/shop/views/user/login.php
+++ themes/green/shop/views/user/login.php
@@ -7,76 +7,27 @@
                     <div class="row">
                     <div class="col-sm-9 col-md-12">
 
-                        <ul class="nav nav-tabs" role="tablist">
-                            
-                            <li role="" >  </li>
-                            <?php if (!$shop_settings->private) {
-    ?>
-                            <!--<li role="presentation" class="active"  ><a href="#register" class="register" aria-controls="register" role="tab" data-toggle="tab"><?= lang('register'); ?></a></li>-->
-                            <?php
-} ?>
-                        </ul>
-
                         <div class="tab-content padding-lg white bordered-light"  style="margin-top:-1px;">
-                            <div class="loginform" style="display:none;" >
-                            
-                                <div class="row w-100 m-auto">
-                                    <div class="col-sm-5">
-                                        <div class="well margin-bottom-no">
-                                          
-                                         <?php include 'login_form.php'; ?>
-                                        </div>
-                                    </div>
-                             <?php if (!$shop_settings->private) {
-     ?>
-                                    <div class="col-sm-7 px-md-5">
-                                        <h4 class="title"><span><?= lang('register_new_account'); ?></span></h4>
-                                        <p>
-                                            <?= lang('register_account_info'); ?>
-                                        </p>
-                                        <a  class="btn btn-primary" id="registerbtn" onclick="registerBtn()" ><?= lang('register'); ?></a>
-                                        
-                                    </div>
-                             <?php
- } ?>
-                                </div>
-                            </div>
 
-                            <?php if (!$shop_settings->private) {
-                            ?>
-                            <div id="register">
-                                <?php 
-                                    $attrib = ['class' => 'validate', 'role' => 'form', 'id' => 'registrationForm'];
-                                    echo form_open('register', $attrib); 
-                                ?>
-                                <div class="row">
-                                    
-                                    <div class="col-sm-6">
-                                        <div class="form-group">
-                                            <?= lang('email', 'email'); ?>
-                                            <div class="controls">
-                                                <input type="email" id="email" name="email" class="form-control" required="required"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    
-                                    <div class="clearfix"></div>
-                                </div>
-                                <div class="mt-2">
-                                <!--<input type="submit" name="register" class="btn btn-primary" id="registerBtn" value="Register" >-->
-                                <button type="button" class="btn btn-primary" id="registerBtnCall">Register</button>
-                                <a id="login" onclick="LoginFn()" name="login" value="Login" class="btn btn-secondary">Already have an account</a>
-                               </div>
-                                <?= form_close(); ?>
-                            </div>
-                            
-                            <?php
-                            } ?>
-                        </div>
-                      
-                      
-                        
+
+
+
+
+
+                            <!-- Paste here -->
+
+
+
+
+
+
+
+                        </div>  
                     </div>
+
+
+
+
 
                     <div class="col-sm-3 col-md-12">
                         <div class="loginRCard px-5">
@@ -87,8 +38,8 @@
                         </div>
                         <h4 class="fw-bold letstart">Let's get started</h4>
                         <div class="logsignBtns mt-3 d-flex justify-content-center">
-                            <button type="button" class="btn  text-white  active">Log in</button>
-                            <button type="button" class="btn  text-white px-4">Sign up</button>
+                            <button type="button" class="btn  text-white  active" onclick="LoginFn(this);" >Log in</button>
+                            <button type="button" class="btn  text-white px-4" onclick="registerBtn(this);" >Sign up</button>
                         </div>
                         <div>
                             <div class="controls logcardinput">
@@ -112,15 +63,17 @@
     // Vanilla Javascript
 
     function LoginFn(){ 
-        $(".register").hide();
-        $("#register").hide();
-        $(".loginform").show();
+        alert('Login Now');
+        //$(".register").hide();
+        //$("#register").hide();
+        //$(".loginform").show();
     }
 
     function registerBtn(){
-        $(".loginform").hide();
-        $(".register").show();
-        $("#register").show();  
+        alert('Register Now');
+        //$(".loginform").hide();
+        //$(".register").show();
+        //$("#register").show();  
     }
 
     /*var input = document.querySelector("#phone");
