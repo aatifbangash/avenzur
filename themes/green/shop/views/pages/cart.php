b89f6c99a2be18109b3060907332a07e5e40483e
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-16 21:59:10 +0300

    'c'

--- themes/green/shop/views/pages/cart.php
+++ themes/green/shop/views/pages/cart.php
@@ -7,21 +7,19 @@
 
                     <div class="col-sm-8">
                         <div class="panel panel-default margin-top-lg">
-                            <!--<div class="panel-heading text-bold">
+                            <div class="panel-heading text-bold">
                                 <i class="fa fa-shopping-cart margin-right-sm"></i> <?= lang('shopping_cart'); ?>
                                 [ <?= lang('items'); ?>: <span id="total-items"></span> ]
                                 <a href="<?= shop_url('products'); ?>" class="pull-right hidden-xs continushopingtxt">
                                     <i class="fa fa-share"></i>
                                     <?= lang('continue_shopping'); ?>
                                 </a>
-                            </div>-->
+                            </div>
                             <div class="panel-body" style="padding:0;">
-                                <!--<div class="cart-empty-msg <?= ($this->cart->total_items() > 1) ? 'hide' : ''; ?>">
+                                <div class="cart-empty-msg <?= ($this->cart->total_items() > 1) ? 'hide' : ''; ?>">
                                     <?= '<h4 class="text-bold">' . lang('empty_cart') . '</h4>'; ?>
-                                </div>-->
-
-
-                                <!--<div class="cart-contents">
+                                </div>
+                                <div class="cart-contents">
                                     <div class="table-responsive">
                                         <table id="cart-table"
                                                class="table table-condensed table-striped table-cart margin-bottom-no">
@@ -39,9 +37,7 @@
                                             <tbody></tbody>
                                         </table>
                                     </div>
-                                </div>-->
-
-
+                                </div>
                             </div>
                             <div class="cart-contents">
                                 <div id="cart-helper" class="panel panel-footer margin-bottom-no">
@@ -84,10 +80,47 @@
                 <!-- cart new dsign -->
                 <div class="row justify-content-between ">
 
-                    
+                    <div class="col-sm-7">
+                    <h3>Shopping Cart</h3>
+                    <div class="row bg-light ps-1 mt-5" style="margin-inline: 3px;padding: 25px;">
+                        <div class="col-md-2">
+                        
+                        <span class="cart-item-image">
+                        
+                        <img style="width: 100px;height: 90px;object-fit: contain;" src="/assets/uploads/121020919.jpg" class="card-img-top" alt="...">
+                        
+                        </span>
+                        </div>
+                        <div class="col-md-10 d-flex flex-column justify-content-between">
+                        <div class="d-flex justify-content-between ">
+                            <h5 class="m-0">Sulfad 1 gm 30 Tab</h5>
+                            <div>
+                            <h4 class="m-0 fw-semibold fs-5" >SAR 65</h4>
+                            <p class="m-0 text-decoration-line-through text-danger text-center fw-semibold mb-4">SAR 10</p>
+                            </div>
+                        </div>
 
-
-
+                        <div class="d-flex justify-content-between align-items-center">
+                            <div>
+                            <a href="#" class="text-red remove-item text-decoration-none text-dark">
+                                <i class="fa fa-trash-o"></i> Remove
+                            </a>
+                            </div>
+                            <div class="quantity text-end py-2 d-flex align-items-center justify-content-between cartQuantity">
+                            <h6 class="my-1 me-2">Quantity</h6>
+                            <span class="plus btn-plus">
+                                <i class="bi bi-plus-circle-fill"></i>
+                            </span>
+                            <span class="fs-6 px-2">1</span>
+                            
+                            <span class="minus btn-minus">
+                                <i class="bi bi-dash-circle-fill"></i>
+                            </span>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
                     <div class="col-sm-5">
                     <div class="text-end">
                         <a class="navbar-brand" href="#">
