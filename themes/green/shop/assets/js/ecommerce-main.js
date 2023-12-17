723cdf5a39103955cec1eb00d08ba1f36466856c
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-17 11:42:41 +0300

    'c'

--- themes/green/shop/assets/js/ecommerce-main.js
+++ themes/green/shop/assets/js/ecommerce-main.js
@@ -851,12 +851,12 @@ $(document).ready(function () {
     var e = $(this).attr("data-id"),
       a = $(".shopping-cart:visible"),
       s = $(this).parents(".card").find("input");
-    console.log(s.val());
+    
     if (typeof s.val() === "undefined") {
       s = $(this).parents(".get-quantity").find("input");
     }
 
-    $('#productPop').show();
+    $('#productPop').modal('show');
 
     //, s = $(this).parents(".product-bottom").find(".quantity-input");
     /*,i=$(this).parents(".product").find("img").eq(0);if(i){i.clone().offset({top:i.offset().top,left:i.offset().left}).css({opacity:"0.5",position:"absolute",height:"150px",width:"150px","z-index":"1000"}).appendTo($("body")).animate({top:a.offset().top+10,left:a.offset().left+10,width:"50px",height:"50px"},400).animate({width:0,height:0},function(){$(this).detach()})}*/
