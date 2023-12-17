9a7a7f55eddec28d2075f63de0175f82fa1e3785
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-17 14:50:39 +0300

    'c'

--- themes/green/shop/views/pages/cart.php
+++ themes/green/shop/views/pages/cart.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); 
 
-//$coupon_disc = $this->cart->coupon_disc();
+$coupon_disc = $this->cart->coupon_disc();
 
 ?>
 
@@ -38,7 +38,7 @@
                                 <input type="text" name="coupon_code"  class="form-control  rounded-0" placeholder="Welcom20">
                                 <button type="submit" class="btn btn-lg primary-buttonAV rounded-0 fw-normal px-1 " style="font-size:14px !important;width: 175px !important;"> 
                                     <?php 
-                                        if($this->cart->coupon_disc()){
+                                        if($coupon_disc){
                                             echo 'Code applied!';
                                         }else{
                                             echo 'Apply Code';
