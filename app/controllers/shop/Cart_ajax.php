6af971366b8f3f5f0fc1fe9af7b424b79ef26741
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-18 15:33:57 +0300

    'c'

--- app/controllers/shop/Cart_ajax.php
+++ app/controllers/shop/Cart_ajax.php
@@ -60,7 +60,7 @@ class Cart_ajax extends MY_Shop_Controller
             $cart_contents = $this->cart->contents();
             foreach ($cart_contents as $item => $val) {
                 //$item['discount'] = ($item['price'] * $discount) / 100;
-                echo $val;
+                print_r($val);
             }
 
             //$this->cart->set_discount($coupon_disc);
