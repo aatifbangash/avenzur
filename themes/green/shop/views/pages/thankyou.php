36acdb9e901717be63355501022c7319178fae8c
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-19 14:39:38 +0300

    'c'

--- themes/green/shop/views/pages/thankyou.php
+++ themes/green/shop/views/pages/thankyou.php
@@ -7,7 +7,7 @@
     <div class="row">
       <div class="col-md-6">
         <h2 class="purpColor">Thank you for your Order</h2>
-        <h4 class="mt-3 fw-semibold fs-4">Order no: 123456777</h4>
+        <h4 class="mt-3 fw-semibold fs-4">Order no: <?= $inv->id; ?></h4>
 
         <div class="my-3 p-3 border">
           <h4 class="m-0 fs-3">Your order is confirmed</h4>
@@ -20,13 +20,14 @@
         <div class="mt-3 p-3 px-4 border">
           <h4 class="m-0 fs-4">Customer information</h4>
 
-          <h4 class="m-0 fs-6 mt-2 py-2 fw-semibold">Contact: +966 2345677</h4>
-          <h4 class="m-0 fs-6 mt-1 fw-semibold">Email: test@gmail.com</h4>
+          <h4 class="m-0 fs-6 mt-2 py-2 fw-semibold">Contact: <?= $customer->phone; ?></h4>
+          <h4 class="m-0 fs-6 mt-1 fw-semibold">Email: <?= $customer->email; ?></h4>
           <div class="my-4 pt-3">
             <h4 class="m-0 fs-4">Shipping address</h4>
 
             <h4 class="m-0 fs-6 mt-2 py-2 fw-semibold">
-              Muhammad room 21, Al radwah, Riyadh, Saudi Arabia
+              <?= $address->first_name. ' '. $address->last_name; ?> <br />
+              <?= $address->line1; ?>
             </h4>
           </div>
           <div class="my-4 pt-3">
