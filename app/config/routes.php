d47982b6c83c45fb8a75b365eb064e802152b5fa
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-20 10:50:20 +0300

    'c'

--- app/config/routes.php
+++ app/config/routes.php
@@ -36,6 +36,7 @@ $route['shop/(:any)/(:any)/(:any)'] = 'shop/shop/$1/$2/$3';
 $route['login']                  = 'main/login';
 $route['login_otp']              = 'main/login_otp';
 $route['verify_phone']           = 'main/verify_phone';
+$route['verify_phone_otp']       = 'main/verify_phone_otp';
 $route['mobile_verify_otp']      = 'main/mobile_verify_otp';
 $route['logout']                 = 'main/logout';
 $route['profile']                = 'main/profile';
