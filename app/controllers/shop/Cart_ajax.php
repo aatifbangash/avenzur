922a01a4847fef0f5458b790f52f03f0daf2919b
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-20 17:11:42 +0300

    'c'

--- app/controllers/shop/Cart_ajax.php
+++ app/controllers/shop/Cart_ajax.php
@@ -290,10 +290,13 @@ class Cart_ajax extends MY_Shop_Controller
         }
         $this->data['address_id'] = '';
         $action = $this->input->get('action');
-        $this->data['addresses']  = $this->loggedIn ? $this->shop_model->getAddresses() : false;
-        $this->data['default_address']  = $this->loggedIn ? $this->shop_model->getDefaultChechoutAddress() : false;
 
-        print_r($this->session->get_userdata('changed_address'));
+        if($this->session->get_userdata('changed_address')){
+            $this->data['default_address']  = $this->session->get_userdata('changed_address');
+        }else{
+            $this->data['default_address']  = $this->loggedIn ? $this->shop_model->getDefaultChechoutAddress() : false;
+        }
+        $this->data['addresses']  = $this->loggedIn ? $this->shop_model->getAddresses() : false;  
        
         if($this->loggedIn && (  $this->data['default_address']->phone == '' || in_array($action, array('addnewaddress', 'editaddress')) ) ) {
         //if($action == 'changeaddress' || empty($this->data['addresses']))  {  
