b23fee712f7d44502837cb36a19272086c313a04
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-20 20:59:48 +0300

    'c'

--- app/controllers/shop/Cart_ajax.php
+++ app/controllers/shop/Cart_ajax.php
@@ -338,7 +338,7 @@ class Cart_ajax extends MY_Shop_Controller
             }else{
                 $this->data['default_address']  = $this->loggedIn ? $this->shop_model->getDefaultChechoutAddress() : false;
             }
-            print_r($this->shop_model->getDefaultChechoutAddress());exit;
+            //print_r($this->shop_model->getDefaultChechoutAddress());exit;
           
             $this->data['paypal']     = $this->shop_model->getPaypalSettings();
             $this->data['skrill']     = $this->shop_model->getSkrillSettings();
