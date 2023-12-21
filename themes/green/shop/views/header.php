3ac1d918ffca22ee6122d5593a97c75cfa6461ff
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-21 10:44:41 +0300

    'cartcontentinmain'

--- themes/green/shop/views/header.php
+++ themes/green/shop/views/header.php
@@ -119,7 +119,7 @@
                     </ul>
                 </div>
                 <div class="dropdown">
-                <button type="button" id="login-btn-trigger" class="btn text-white dropdown-toggle px-0 border-0" data-bs-toggle="dropdown" aria-expanded="false" data-bs-auto-close="outside">
+                <button type="button" id="login-btn-trigger" class="btn text-white dropdown-toggle px-0 border-0" aria-expanded="false" data-bs-auto-close="outside">
                   <?php
                     if ($loggedIn) {
                       ?>
