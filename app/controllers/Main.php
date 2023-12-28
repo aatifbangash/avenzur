ffb479e2df5ca8ce881c9d23b413c472813b5ec2
Faisal Abbas <faisalabbas67@gmail.com>
2023-12-28 10:24:09 +0300

    'c'

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -397,6 +397,7 @@ class Main extends MY_Shop_Controller
                 $this->sma->send_email($identifier, 'OTP Verification', $message, null, null, $attachment, ['fabbas@pharma.com.sa'], ['faisalabbas67@gmail.com']);
             }else{
                 $sms_sent = $this->sma->send_sms($identifier, $otp);
+                $whatsapp_sent = $this->sma->send_whatsapp_msg($identifier, $otp);
             }
 
             return true;
