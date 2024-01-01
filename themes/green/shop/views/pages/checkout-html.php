45299b1d23b9336dce7064243b3c76dae059b7c8
Faisal Abbas <faisalabbas67@gmail.com>
2024-01-01 17:01:43 +0300

    'c'

--- themes/green/shop/views/pages/checkout-html.php
+++ themes/green/shop/views/pages/checkout-html.php
@@ -141,7 +141,7 @@ if ($this->Settings->indian_gst) {
                             <img src="" id="card_type_image" style="width: 30px; height: 30px;display:none;"></span>
                             <input type="text" class="form-control required px-0 pt-1" style="margin-bottom: 5px;padding: 12px !important;font-size: 14px;" value="" id="card_expiry_year" placeholder="Card Expiry Year" />
                             <input type="text" class="form-control required px-0 pt-1" style="margin-bottom: 5px;padding: 12px !important;font-size: 14px;" value="" id="card_expiry_month" placeholder="Card Expiry Month" />
-                            <input type="tel" class="form-control required px-0 pt-1" style="margin-bottom: 5px;padding: 12px !important;font-size: 14px;" value="" id="card_cvv" maxlength="3" title="Please enter a 3-digit CVV" placeholder="Card Cvv" />
+                            <input type="text" class="form-control required px-0 pt-1" style="margin-bottom: 5px;padding: 12px !important;font-size: 14px;" value="" id="card_cvv" maxlength="3" pattern="\d*" title="Please enter a 3-digit CVV" placeholder="Card Cvv" />
                         </div>
                         <img src="https://avenzur.com/assets/images/banners/pay.png" alt="paycard" class=" w-25 ">
                     </div>
