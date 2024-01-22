f7f8b2e84e6df6913adb4c018b1e300133e2906a
aleem <aleemktk@gmail.com>
2024-01-22 16:57:21 +0300

    product notificaiton changes

--- app/controllers/admin/Cron.php
+++ app/controllers/admin/Cron.php
@@ -34,4 +34,53 @@ class Cron extends MY_Controller
             }
         }
     }
+
+     public function products_notification()
+    {
+//        SET CRONJOB:- php index.php admin/cron products_notification
+        $this->db->select('sn.id, sn.is_notified, sn.email, p.id as pid, p.name as pname, p.slug pslug, p.quantity');
+        $this->db->from('sma_products p');
+        $this->db->join('sma_out_of_stock_notify sn', 'p.id = sn.product_id');
+        $this->db->where('sn.product_id >', 0);
+        $this->db->where('sn.is_notified', 0);
+        $this->db->where('p.quantity >', 0);
+
+        $query = $this->db->get();
+
+        $result = $query->result();
+        if (!empty($result)) {
+            $subject = "New Product Added to Stock";
+
+            foreach ($result as $row) {
+
+                $body = "<p>Hello,</p>";
+                $body .= "<p>We are excited to inform you that new products have been added to our stock. You can now visit our website and explore the latest additions to our inventory.</p>";
+                $body .= "<p><strong>Product Information:</strong></p>";
+                $body .= "<ul>";
+                $body .= "<li><strong>Product Name:</strong> {$row->pname}</li>";
+                $body .= "</ul>";
+                $body .= "<p>Click the following link to view the products and make a purchase or add them to your cart: <a href='" . site_url("product/{$row->pslug}") . "'>View Product</a></p>";
+                $body .= "<p>Thank you for choosing our services!</p>";
+             //echo $body;
+                // Send the email notification
+                $sent = true;
+             $sent = $this->sma->send_email(
+                   $row->email,
+                   $subject,
+                   $body,
+                   "info@avenzur.com",
+                   "Avenzur"
+               );
+
+                if($sent) {
+                    $this->db
+                        ->set('is_notified', 1)
+                        ->set('date_updated', date('Y-m-d H:i:s'))
+                        ->where(['id' => $row->id, 'is_notified' => 0])
+                        ->update('out_of_stock_notify');
+                }
+            }
+        }
+    }
+
 }
