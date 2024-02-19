34b468197466ad50f91ee451b5c4a4c799d08bc7
Faisal Abbas <faisalabbas67@gmail.com>
2024-02-19 17:32:37 +0300

    saudi day new banner link change

--- themes/green/shop/views/index.php
+++ themes/green/shop/views/index.php
@@ -179,7 +179,7 @@
     <section class="side-banner section-marg-top">
       <div class="container container-max-width">
         <div class="sideBannerImg">
-          <a href="<?= site_url('shop/products?special_product=yes'); ?>">
+          <a href="<?= site_url('shop/products?promo=yes'); ?>">
             <img loading="lazy" src="<?= base_url('assets/images/banners/special_offers_en.jpg'.'?timestamp='.time()); ?>" alt="placeholder" class="w-100" />
           </a>
         </div>
