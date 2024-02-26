43f3d0ffcf4f3a4619f155bfdafcc68bd5ffdf5f
Faisal Abbas <faisalabbas67@gmail.com>
2024-02-26 12:13:15 +0300

    whatsapp order confirmation

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -501,7 +501,6 @@ class Main extends MY_Shop_Controller
                 $validate = $this->shop_model->validate_otp($identity, $otp);
                 if ($validate) {
                     if ($this->form_validation->run('auth/login') == true) {
-                        echo 'Authenticated...';exit;
                         $remember = true;
                         if (!empty($company_data->email)) {
                             $login_column = $company_data->email;
@@ -512,6 +511,7 @@ class Main extends MY_Shop_Controller
                         $this->shop_model->activate_user($login_column);
 
                         if ($this->ion_auth->login($login_column, '12345', $remember)) {
+                            echo 'LoggedIn...';exit;
                             // login success
                             if ($type == 'mobile') {
                                 $this->shop_model->updateCompany($company_data->id, ['mobile_verified' => 1]);
@@ -532,7 +532,7 @@ class Main extends MY_Shop_Controller
                                 redirect($referrer);
                             }
                         } else {
-                            echo 'Cannot Authenticate...';exit;
+                            echo 'Cannot LoggedIn...';exit;
                             $this->session->set_flashdata('error', $this->ion_auth->errors());
                             //redirect('login');
                             $referrer = ($this->session->userdata('requested_page') && $this->session->userdata('requested_page') != 'admin') ? $this->session->userdata('requested_page') : '/';
