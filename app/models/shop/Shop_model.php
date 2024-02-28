d74ba0a57e216038175f53d44c5e6867dc4f7c9d
Faisal Abbas <faisalabbas67@gmail.com>
2024-02-28 13:37:42 +0300

    order debugging

--- app/models/shop/Shop_model.php
+++ app/models/shop/Shop_model.php
@@ -153,7 +153,9 @@ class Shop_model extends CI_Model
         }
 
         $this->db->trans_start();
+        print_r($data);exit;
         if ($this->db->insert('sales', $data)) {
+            echo 'sale done';exit;
             $sale_id = $this->db->insert_id();
             echo 'SaleId: '.$sale_id;exit;
             $this->site->updateReference('so');
