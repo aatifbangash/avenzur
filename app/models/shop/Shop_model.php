9f5c63d1c029ad5584245701e110df0d6c3f33a3
Faisal Abbas <faisalabbas67@gmail.com>
2024-02-28 13:40:21 +0300

    order debugging

--- app/models/shop/Shop_model.php
+++ app/models/shop/Shop_model.php
@@ -152,8 +152,10 @@ class Shop_model extends CI_Model
             $data['address_id'] = $this->db->insert_id();
         }
 
-        $this->db->trans_start();
-        print_r($data);exit;
+        //$this->db->trans_start();
+        //print_r($data);exit;
+        $this->db->insert('sales', $data);
+        echo $this->db->last_query();exit;
         if ($this->db->insert('sales', $data)) {
             echo 'sale done';exit;
             $sale_id = $this->db->insert_id();
