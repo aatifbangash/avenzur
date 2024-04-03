c3272a220b706cdb9259aa5a7fdc94a6a9f0bb45
Faisal Abbas <faisalabbas67@gmail.com>
2024-04-03 14:23:01 +0300

    eid banners upload

--- themes/green/shop/views/footer.php
+++ themes/green/shop/views/footer.php
@@ -221,9 +221,9 @@
         carouselItems[1].querySelector('img').src = site.site_url + '/assets/uploads/' + imageName_2 + '.jpg?timestamp=' + Date.now();
         carouselItems[2].querySelector('img').src = site.site_url + '/assets/uploads/' + imageName_3 + '.jpg?timestamp=' + Date.now();
         // set english promo banner
-        document.getElementById('promo-banner-1').src = site.site_url + '/assets/images/banners/ramadan_offers_en.jpg?timestamp=' + Date.now();
+        document.getElementById('promo-banner-1').src = site.site_url + '/assets/images/banners/eid_offers_en.jpg?timestamp=' + Date.now();
       }else{
-        document.getElementById('promo-page-banner-1').src = site.site_url + '/assets/images/banners/promo-page-banner-ramadan-en.jpg?timestamp=' + Date.now();
+        document.getElementById('promo-page-banner-1').src = site.site_url + '/assets/images/banners/promo-page-banner-eid-en.jpg?timestamp=' + Date.now();
       }
 
     } else if (newLang === 'ar') {
@@ -272,9 +272,9 @@
         carouselItems[2].querySelector('img').src = site.site_url + '/assets/uploads/' + imageName_3 + '.jpg?timestamp=' + Date.now();
 
         // set arabic promo banner
-        document.getElementById('promo-banner-1').src = site.site_url + '/assets/images/banners/ramadan_offers_ar.jpg?timestamp=' + Date.now();
+        document.getElementById('promo-banner-1').src = site.site_url + '/assets/images/banners/eid_offers_ar.jpg?timestamp=' + Date.now();
       }else{
-        document.getElementById('promo-page-banner-1').src = site.site_url + '/assets/images/banners/promo-page-banner-ramadan-ar.jpg?timestamp=' + Date.now();
+        document.getElementById('promo-page-banner-1').src = site.site_url + '/assets/images/banners/promo-page-banner-eid-ar.jpg?timestamp=' + Date.now();
       }
     }
 
