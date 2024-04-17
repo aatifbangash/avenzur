443b07f1c012bd8f9318f9d31e8afc674960eab7
Faisal Abbas <faisalabbas67@gmail.com>
2024-04-17 10:57:52 +0300

    'adminpurchasesdebug'

--- app/controllers/admin/Purchases.php
+++ app/controllers/admin/Purchases.php
@@ -896,6 +896,8 @@ class Purchases extends MY_Controller
             $attachments        = $this->attachments->upload();
             $data['attachment'] = !empty($attachments);
             //$this->sma->print_arrays($data, $products);exit;
+        }else{
+            echo 'Validation failed...';exit;
         }
 
         if ($this->form_validation->run() == true && $this->purchases_model->updatePurchase($id, $data, $products, $attachments)) {
