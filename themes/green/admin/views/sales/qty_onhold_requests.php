d36978767cd0ae0e5215cc0c777de126bda2f091
Faisal Abbas <faisalabbas67@gmail.com>
2024-04-29 16:58:21 +0300

    virtual pharmacy details capture

--- themes/green/admin/views/sales/qty_onhold_requests.php
+++ themes/green/admin/views/sales/qty_onhold_requests.php
@@ -33,6 +33,7 @@
                             <th>Quantity</th>
                             <th>Status</th>
                             <th>Virtual Pharma</th>
+                            <td>Date</td>
                             
                             <th style="width:80px; text-align:center;"><?= lang('actions'); ?></th>
                         </tr>
@@ -53,6 +54,7 @@
                             <td><?php echo $item->quantity;?></td>
                             <td><?php echo $item->status;?></td>
                             <td><?php echo $item->virtual_pharmacy == 0 ? 'No': 'Yes';?></td>
+                            <td><?php echo $item->date_created;?></td>
                             <td><a href="<?php echo admin_url('sales/delete_qty_onhold/' . $item->id); ?>" class="tip" title="" data-original-title="Delete Request"><i class="fa fa-trash-o"></i></a></td>
                            
                         </tr>
