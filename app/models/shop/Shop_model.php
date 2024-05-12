c24c7c216fc392e420ff2b57c4503075baf4877f
Faisal Abbas <faisalabbas67@gmail.com>
2024-05-12 18:26:08 +0300

    ecommerce change quantity revert inventory movement

--- app/models/shop/Shop_model.php
+++ app/models/shop/Shop_model.php
@@ -1290,12 +1290,11 @@ class Shop_model extends CI_Model
         {$this->db->dbprefix('products')}.image as image, 
         {$this->db->dbprefix('products')}.slug as slug, 
         {$this->db->dbprefix('products')}.price,
-        {$this->db->dbprefix('warehouses_products')}.quantity as old_quantity, 
+        {$this->db->dbprefix('warehouses_products')}.quantity as quantity, 
         {$this->db->dbprefix('products')}.type, 
         {$this->db->dbprefix('products')}.tax_rate as taxRateId, 
         {$this->db->dbprefix('products')}.tax_method,
         {$this->db->dbprefix('products')}.quantity as product_quantity,
-        {$this->db->dbprefix('inventory_movements')}.quantity as quantity,
         promotion, 
         promo_price, 
         start_date, 
@@ -1308,7 +1307,6 @@ class Shop_model extends CI_Model
             ->from('products')
             ->join('tax_rates t', 'products.tax_rate = t.id', 'left')
             ->join('warehouses_products', 'products.id=warehouses_products.product_id', 'left')
-            ->join('inventory_movements', 'products.id=inventory_movements.product_id', 'left')
             ->join('categories', 'products.category_id=categories.id', 'left')
             ->join('brands', 'products.brand=brands.id', 'left')
             ->join('product_reviews pr', 'products.id=pr.product_id', 'left');
@@ -1387,7 +1385,7 @@ class Shop_model extends CI_Model
                 $this->db->where('price <=', $filters['max_price']);
             }
             if (!empty($filters['in_stock'])) {
-                $this->db->group_start()->where('inventory_movements.quantity >=', 1)->or_where('type !=', 'standard')->group_end();
+                $this->db->group_start()->where('warehouses_products.quantity >=', 1)->or_where('type !=', 'standard')->group_end();
             }
 
             if(!empty($filters['promo']) || !empty($filters['special_product'])){
@@ -1850,23 +1848,6 @@ class Shop_model extends CI_Model
         $this->db->insert('out_of_stock_notify', $data); // 'sma_notify' is the name of your database table
 
         return $this->db->insert_id(); // Return the ID of the inserted row
-    }
-
-    public function getBrandsByCategoy($category_id=0){
-       
-        if($category_id > 0) {
-            $this->db->select('' . $this->db->dbprefix('brands') . '.*')
-                ->join('products', 'brands.id=products.brand', 'left')
-                ->group_by('brands.id');
-            return $this->db->get('brands')->row();
-        }else {
-            $this->db->select('' . $this->db->dbprefix('brands') . '.*')
-            ->join('products', 'brands.id=products.brand', 'left')
-            ->where('products.category_id', $category_id)
-            ->group_by('brands.id');
-        return $this->db->get('brands')->result_array();
-        }
-
     }
 
 }
