aa18d0dec5156b23d155dece7019415c32ac40ca
Faisal Abbas <faisalabbas67@gmail.com>
2024-05-15 14:27:19 +0300

    products search quantity calculate from inventory

--- app/models/shop/Shop_model.php
+++ app/models/shop/Shop_model.php
@@ -1294,7 +1294,7 @@ class Shop_model extends CI_Model
         {$this->db->dbprefix('products')}.type, 
         {$this->db->dbprefix('products')}.tax_rate as taxRateId, 
         {$this->db->dbprefix('products')}.tax_method,
-        {$this->db->dbprefix('products')}.quantity as product_quantity,
+        SUM(sma_inventory_movements.quantity) as product_quantity,
         promotion, 
         promo_price, 
         start_date, 
@@ -1309,7 +1309,8 @@ class Shop_model extends CI_Model
             ->join('warehouses_products', 'products.id=warehouses_products.product_id', 'left')
             ->join('categories', 'products.category_id=categories.id', 'left')
             ->join('brands', 'products.brand=brands.id', 'left')
-            ->join('product_reviews pr', 'products.id=pr.product_id', 'left');
+            ->join('product_reviews pr', 'products.id=pr.product_id', 'left')
+            ->join('sma_inventory_movements', 'products.id=sma_inventory_movements.product_id', 'left');
         if ($this->shop_settings->warehouse > 0) {
             $this->db->where('warehouses_products.warehouse_id', $this->shop_settings->warehouse);
         }
