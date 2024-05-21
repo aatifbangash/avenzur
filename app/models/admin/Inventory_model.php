b81d2813e84040bdc0bb2d7193157a9bcf8c55dd
aleem <aleemktk@gmail.com>
2024-05-21 22:15:42 +0300

    fixes in inventory model

--- app/models/admin/Inventory_model.php
+++ app/models/admin/Inventory_model.php
@@ -60,11 +60,12 @@ class Inventory_model extends CI_Model {
     // Function to calculate current stock for a specific product at a given location
     public function get_current_stock($product_id, $location_id, $batch_no = null)
     {
+       
         $this->db->select('SUM(quantity) as total_quantity');
         $this->db->from('inventory_movements');
         $this->db->where('product_id', $product_id);
 
-        if($batch_no != 'null'){
+        if($batch_no != null){
             $this->db->where('batch_number', $batch_no);
         }
 
