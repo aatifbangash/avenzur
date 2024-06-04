96689263b7b85325f7c792acfaf03f18ee82678b
Faisal Abbas <faisalabbas67@gmail.com>
2024-06-04 11:30:38 +0300

    fix/login redirect issue

--- app/controllers/Main.php
+++ app/controllers/Main.php
@@ -691,6 +691,7 @@ class Main extends MY_Shop_Controller
 
     public function login()
     {
+        redirect('/');
         $this->form_validation->set_rules('identity', lang('Email or Mobile'), 'required');
 
         if ($this->form_validation->run('') == true) {
