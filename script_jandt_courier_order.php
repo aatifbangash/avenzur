20243c6180e76c55356aa84a51ad5abe0c6ab3b0
Faisal Abbas <faisalabbas67@gmail.com>
2024-06-05 10:03:24 +0300

    fix/jandt issue fix

--- script_jandt_courier_order.php
+++ script_jandt_courier_order.php
@@ -355,7 +355,7 @@ if ($result_sales->num_rows > 0) {
             $result_sales_items = $stmt_items->get_result();
 
             //sending delivered email for status update
-            if ($tracking_status == 'Sign scan') { // Assuming 'Sign scan' indicates delivered
+            if ($tracking_status == 'Delivery scan') { // Assuming 'Sign scan' indicates delivered
 
                 $messageBody = '
                 <!DOCTYPE html>
