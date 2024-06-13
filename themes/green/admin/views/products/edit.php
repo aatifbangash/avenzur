694b0f1b8d5d9d6a516aa6f985ac00e90a578fe1
Jahanzaib khan <jahanzaib@avenzur.com>
2024-06-13 14:15:49 +0500

    some modifications

--- themes/green/admin/views/products/edit.php
+++ themes/green/admin/views/products/edit.php
@@ -401,13 +401,13 @@ if (!empty($variants)) {
                         <input id="images" type="file" data-browse-label="<?= lang('browse'); ?>" name="userfile[]" multiple="true" data-show-upload="false"
                                data-show-preview="true" class="form-control file" accept="image/*">
                     </div>
-                    <div id="multiimages" class="padding10">
+                    <div id="multiimages" class="row">
                                     <?php if (!empty($images)) {
                                             // echo '<a class="img-thumbnail" data-toggle="lightbox" data-gallery="multiimages" data-parent="#multiimages" href="' . base_url() . 'assets/uploads/' . $product->image . '" style="margin-right:5px;"><img class="img-responsive" src="' . base_url() . 'assets/uploads/thumbs/' . $product->image . '" alt="' . $product->image . '" style="width:' . $Settings->twidth . 'px; height:' . $Settings->theight . 'px;" /></a>';
                                             foreach ($images as $ph) {
                                                 if ($ph->photo != $product->image)
                                                 {
-                                                    echo '<div class="gallery-image"><a class="img-thumbnail" data-toggle="lightbox" data-gallery="multiimages" data-parent="#multiimages" href="' . base_url() . 'assets/uploads/' . $ph->photo . '" style="margin-right:5px;"><img class="img-responsive" src="' . base_url() . 'assets/uploads/thumbs/' . $ph->photo . '" alt="' . $ph->photo . '" style="width:' . $Settings->twidth . 'px; height:' . $Settings->theight . 'px;" /></a>';
+                                                    echo '<div class="gallery-image "><a class="img-thumbnail" data-toggle="lightbox" data-gallery="multiimages" data-parent="#multiimages" href="' . base_url() . 'assets/uploads/' . $ph->photo . '" style="margin-right:5px;"><img class="img-responsive" src="' . base_url() . 'assets/uploads/thumbs/' . $ph->photo . '" alt="" style="width: 100px; height: 100px;" /></a>';
                                                     if ($Owner || $Admin || $GP['products-edit']) {
                                                         echo '<a href="#" class="delimg" data-item-id="' . $ph->id . '"><i class="fa fa-times"></i></a>';
                                                     }
