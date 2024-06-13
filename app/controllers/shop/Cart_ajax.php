5f811f2d5dbafea52bb5199d2ef115cd56fbb42e
Faisal Abbas <faisalabbas67@gmail.com>
2024-06-13 11:17:53 +0300

    fix/invalid coupon message

--- app/controllers/shop/Cart_ajax.php
+++ app/controllers/shop/Cart_ajax.php
@@ -271,7 +271,8 @@ class Cart_ajax extends MY_Shop_Controller
             $this->session->set_flashdata('message', 'Coupon Code Applied');
             redirect('cart');
         }else{
-            $this->session->set_flashdata('error', 'Invalid Coupon Code');
+            $this->session->set_flashdata(['error' => 1, 'message' => 'Invalid Coupon Code']);
+            //$this->sma->send_json(['error' => 1, 'message' => 'Invalid Coupon Code']);
             redirect('cart');
         }
     }
