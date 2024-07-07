38d8e537f8548c66cc33034eb242656c7efe772d
aleem <aleemktk@gmail.com>
2024-07-07 14:09:41 +0300

    changes for purchases view

--- themes/blue/admin/views/purchases/view.php
+++ themes/blue/admin/views/purchases/view.php
@@ -194,10 +194,10 @@
                                 <td style="width: 80px; text-align:center; vertical-align:middle;"><?= $row->hsn_code ?: ''; ?></td>
                                 <?php
                             } ?>
-                                <td style="width: 120px; text-align:center; vertical-align:middle;"><?= $this->sma->formatQuantity($row->unit_quantity) . ' ' . $row->product_unit_code; ?></td>
+                                <td style="width: 120px; text-align:center; vertical-align:middle;"><?= $this->sma->formatQuantity($row->unit_quantity) ; ?></td>
                                 <?php
                                 if ($inv->status == 'partial') {
-                                    echo '<td style="text-align:center;vertical-align:middle;width:120px;">' . $this->sma->formatQuantity($row->quantity_received) . ' ' . $row->product_unit_code . '</td>';
+                                    echo '<td style="text-align:center;vertical-align:middle;width:120px;">' . $this->sma->formatQuantity($row->quantity_received)  . '</td>';
                                 }
                                 ?>
                                 <td style="text-align:right; width:120px; padding-right:10px;">
