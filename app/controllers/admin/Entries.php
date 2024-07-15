46901de8b72b7944e1a429204e83cfc97501f988
mushtaq41 <mushtaq@avenzur.com>
2024-07-15 14:47:23 +0500

    Apply pagination to entries listing page

--- app/controllers/admin/Entries.php
+++ app/controllers/admin/Entries.php
@@ -23,6 +23,13 @@ class Entries extends MY_Controller
     
 	public function index() {
 
+		$this->load->library('pagination'); 
+		$config['base_url'] = admin_url('entries'); 
+		$config['total_rows'] = $this->count_entries();
+		$config['per_page'] = 100; 
+		$this->pagination->initialize($config); 
+        $this->data['pagination_links']=  $this->pagination->create_links();  
+		
 		$eid         = $this->input->get('eid');
         $tran_number   = $this->input->get('tran_number');
 		$start_date     = $this->input->get('start_date');
@@ -32,14 +39,6 @@ class Entries extends MY_Controller
 			$start_date = $this->sma->fld($start_date);
 			$end_date   = $this->sma->fld($end_date);
 		}
-		
-		// $conditions = array(); // conditions if any
-		// if ($conditions)
-		// {
-		// 	// if any conditions apply where clause
-		// 	$this->db->where($conditions);
-		// }
-
 		if ($start_date && $start_date != "0000-00-00") {
             $this->db->where('date >=', $start_date);       
 
@@ -47,22 +46,17 @@ class Entries extends MY_Controller
                 $this->db->where('date <=', $end_date);
             }
         }
-
-
 		if(!empty($eid)){
 			$this->db->where("id LIKE '%$eid%'");	
 		}
-
 		if(!empty($tran_number)){
 			$this->db->where("number LIKE '%$tran_number%'");	
 		}
-
 		// select all entries
-		$query = $this->db->get('sma_accounts_entries');
-
-		
-
-
+		// 	$query = $this->db->get('sma_accounts_entries');
+		$page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+		$query = $this->db->limit($config['per_page'], $page)->get('accounts_entries');
+		 
 		// pass an array of all entries to view
 		$this->data['entries'] = $query->result_array();
 		
@@ -70,6 +64,42 @@ class Entries extends MY_Controller
 		$bc  = [['link' => base_url(), 'page' => lang('home')], ['link' => admin_url('Entries'), 'page' => lang('Entries')], ['link' => '#', 'page' => lang('Entries')]];
         $meta = ['page_title' => lang('Entries'), 'bc' => $bc];
         $this->page_construct('accounts/entries_index', $meta, $this->data);
+	}
+
+	public function count_entries(){
+
+		$eid         = $this->input->get('eid');
+        $tran_number   = $this->input->get('tran_number');
+		$start_date     = $this->input->get('start_date');
+        $end_date     = $this->input->get('end_date'); 
+		
+		$this->db->select(' COUNT(id) as total_record');
+		$this->db->from('sma_accounts_entries');  
+
+		if ($start_date) {
+			$start_date = $this->sma->fld($start_date);
+			$end_date   = $this->sma->fld($end_date);
+		}
+		if ($start_date && $start_date != "0000-00-00") {
+            $this->db->where('date >=', $start_date);       
+
+            if ($end_date  && $end_date != "0000-00-00") {
+                $this->db->where('date <=', $end_date);
+            }
+        }
+		if(!empty($eid)){
+			$this->db->where("id LIKE '%$eid%'");	
+		}
+		if(!empty($tran_number)){
+			$this->db->where("number LIKE '%$tran_number%'");	
+		}
+
+		$query = $this->db->get();
+		 // echo $this->db->last_query(); exit;
+		$row = $query->row();
+		$count = $row->total_record;
+		return $count;   
+
 	}
 
 	public function ledgerList($entrytypeLabel, $searchTerm = null, $selectedLedgers = array()) {
