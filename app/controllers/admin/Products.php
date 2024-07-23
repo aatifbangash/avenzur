df3ec52508bf5ad4cbda4c652734c893c547b7b2
mushtaq41 <mushtaq@avenzur.com>
2024-07-23 12:26:47 +0500

    fix product quantity on listing add and detail page

--- app/controllers/admin/Products.php
+++ app/controllers/admin/Products.php
@@ -3458,31 +3458,36 @@ class Products extends MY_Controller
         </div></div>';
         $this->load->library('datatables');
         if ($warehouse_id) {
+             // wp.rack as rack replaced with 1 as rack
             $this->datatables
-            ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code,{$this->db->dbprefix('products')}.sequence_code as sequence_code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, SUM(wp.quantity) as quantity, {$this->db->dbprefix('units')}.code as unit, wp.rack as rack, alert_quantity", false)
+            ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code,{$this->db->dbprefix('products')}.sequence_code as sequence_code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, SUM(wp.quantity) as quantity, {$this->db->dbprefix('units')}.code as unit, 1 as rack, alert_quantity", false)
             ->from('products');
             if ($this->Settings->display_all_products) {
-                $this->datatables->join('warehouses_products wp', "wp.product_id=products.id AND wp.warehouse_id={$warehouse_id}", 'left');
+                $this->datatables->join('inventory_movements wp', "wp.product_id=products.id AND wp.location_id={$warehouse_id}", 'left');
+                //$this->datatables->join('warehouses_products wp', "wp.product_id=products.id AND wp.warehouse_id={$warehouse_id}", 'left');
             // $this->datatables->join("( SELECT product_id, quantity, rack from {$this->db->dbprefix('warehouses_products')} WHERE warehouse_id = {$warehouse_id}) wp", 'products.id=wp.product_id', 'left');
             } else {
-                $this->datatables->join('warehouses_products wp', 'products.id=wp.product_id', 'left')
-                ->where('wp.warehouse_id', $warehouse_id)
-                ->where('wp.quantity !=', 0);
+                $this->datatables->join('inventory_movements wp', "wp.product_id=products.id", 'left')
+                ->where('wp.location_id', $warehouse_id)
+                ;
+                // $this->datatables->join('warehouses_products wp', 'products.id=wp.product_id', 'left')
+                // ->where('wp.warehouse_id', $warehouse_id)
+                // ->where('wp.quantity !=', 0);
             }
             $this->datatables->join('categories', 'products.category_id=categories.id', 'left')
             ->join('units', 'products.unit=units.id', 'left')
             ->join('brands', 'products.brand=brands.id', 'left')
             ->group_by("products.id");
         } else {
-
+            // wp.rack as rack replaced with 1 as rack
             $this->datatables
-                ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code,{$this->db->dbprefix('products')}.sequence_code as sequence_code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, SUM(wp.quantity) as quantity, {$this->db->dbprefix('units')}.code as unit, wp.rack as rack, alert_quantity", false)
-                ->from('products');
+                ->select($this->db->dbprefix('products') . ".id as productid, {$this->db->dbprefix('products')}.image as image, {$this->db->dbprefix('products')}.code as code,{$this->db->dbprefix('products')}.sequence_code as sequence_code, {$this->db->dbprefix('products')}.name as name, {$this->db->dbprefix('brands')}.name as brand, {$this->db->dbprefix('categories')}.name as cname, cost as cost, price as price, SUM(wp.quantity) as quantity, {$this->db->dbprefix('units')}.code as unit, 1 as rack, alert_quantity", false)
+                ->from('products'); 
+                $this->datatables->join('inventory_movements wp', "wp.product_id=products.id", 'left');
                 if ($this->Settings->display_all_products) {
-                    $this->datatables->join('warehouses_products wp', "wp.product_id=products.id", 'left');
+                   //  $this->datatables->join('warehouses_products wp', "wp.product_id=products.id", 'left'); 
                 } else {
-                    $this->datatables->join('warehouses_products wp', 'products.id=wp.product_id', 'left')
-                    ->where('wp.quantity !=', 0);
+                   //  $this->datatables->join('warehouses_products wp', 'products.id=wp.product_id', 'left')->where('wp.quantity !=', 0);     
                 }
                 $this->datatables->join('categories', 'products.category_id=categories.id', 'left')
                 ->join('units', 'products.unit=units.id', 'left')
