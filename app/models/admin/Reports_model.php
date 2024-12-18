d11def410e04d404a9fdbf94ce7f284a4c676e5b
Faisal Abbas <faisalabbas67@gmail.com>
2024-12-18 13:19:07 +0300

    stock report fixes

--- app/models/admin/Reports_model.php
+++ app/models/admin/Reports_model.php
@@ -1960,7 +1960,7 @@ class Reports_model extends CI_Model
             $stockQuery .= " AND inv.product_id = '{$item}' ";
         }
 
-        $stockQuery .= "GROUP BY inv.product_id, inv.avz_item_code HAVING quantity > 0";
+        $stockQuery .= " GROUP BY inv.product_id, inv.avz_item_code HAVING quantity > 0";
         $stockResults = $this->db->query($stockQuery);
         // echo $this->db->last_query(); exit; 
         if ($stockResults->num_rows() > 0) {
@@ -2008,7 +2008,7 @@ class Reports_model extends CI_Model
             $stockQuery .= " AND inv.product_id = '{$item}' ";
         }
 
-        $stockQuery .= "GROUP BY inv.product_id, inv.avz_item_code HAVING quantity > 0 ORDER BY p.id DESC LIMIT {$per_page} OFFSET {$offset}";
+        $stockQuery .= " GROUP BY inv.product_id, inv.avz_item_code HAVING quantity > 0 ORDER BY p.id DESC LIMIT {$per_page} OFFSET {$offset}";
         $stockResults = $this->db->query($stockQuery);
         // echo $this->db->last_query(); exit; 
         if ($stockResults->num_rows() > 0) {
