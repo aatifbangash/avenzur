93ac921e07a6cc9aa9562d2c32927eeca9f9547c
Faisal Abbas <faisalabbas67@gmail.com>
2025-04-23 10:39:21 +0300

    manual rasd transfers push

--- app/controllers/admin/Transfers.php
+++ app/controllers/admin/Transfers.php
@@ -510,7 +510,7 @@ class Transfers extends MY_Controller
 
     public function manual_transfer_rasd(){
 
-        $transfer_id = 567;
+        $transfer_id = 569;
         //$transfer_id = 171;
         $data = (array) $this->transfers_model->getTransferByID($transfer_id);
         $products_obj = $this->transfers_model->getAllTransferItems($transfer_id, $data['status']);
