b4c698e5612765254038ac98f5f8d1d19acd0e24
Faisal Abbas <faisalabbas67@gmail.com>
2025-05-05 16:11:45 +0300

    rasd cronjob issue fix

--- script_process_rasd_pharmacy_sales.php
+++ script_process_rasd_pharmacy_sales.php
@@ -49,7 +49,7 @@ if ($result_unprocessed_sales->num_rows > 0) {
             ];
 
             $payload = create_payload_for_gln($serial_no['pharmacy_gln'], $item);
-            echo $payload;exit;
+            echo '<pre>';print_r($payload);exit;
             $response = api_call($payload, $headers);
             
             $response_body = $response['body'];
