3de0bc90f8a497bb39b9807a6441a1926b7cdbe0
Faisal Abbas <faisalabbas67@gmail.com>
2025-05-07 12:00:28 +0300

    company logic with subdomain

--- app/config/primary_db.php
+++ app/config/primary_db.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+$url = $_SERVER['REQUEST_URI'];
+
 if (isset($_COOKIE['companyID']) || isset($_GET['cid']) ) {
 
     // Connection to Primary DB Start
@@ -35,6 +37,34 @@ if (isset($_COOKIE['companyID']) || isset($_GET['cid']) ) {
         die("Invalid request");
     }
     $connection->close();
+}else if(strpos($url, '/admin/') !== false){
+    // Connection to Primary DB Start
+    $connection = new mysqli($hostname, $username, $password, $database);
+    if ($connection->connect_error) {
+        die("Connection failed: " . $connection->connect_error);
+    }
+
+    $host = $_SERVER['HTTP_HOST']; // e.g., retaj.avenzur.com
+    $parts = explode('.', $host);
+
+    // Check for subdomain existence
+    if (count($parts) > 2) {
+        $company_name = $parts[0]; // 'retaj'
+
+        $sql = "SELECT * FROM sma_multi_company m 
+                    Inner Join `sma_dbs` d 
+                        ON d.id = m.db_id 
+                WHERE is_used = 1 AND is_primary <> 1 AND m.company = {$company_name} limit 1";
+        $result = $connection->query($sql);
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $username = trim($row["db_user"]);
+            $password = trim($row["db_pass"]);
+            $database = trim($row["db_name"]);
+        }
+
+        $connection->close();
+    }
 }
 
 // Connection to Primary DB End
