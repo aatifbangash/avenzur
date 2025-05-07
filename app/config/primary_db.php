8cc751d151aa013f965a61f063d9af1659cb4460
Faisal Abbas <faisalabbas67@gmail.com>
2025-05-07 11:52:23 +0300

    company logic with subdomain

--- app/config/primary_db.php
+++ app/config/primary_db.php
@@ -35,6 +35,30 @@ if (isset($_COOKIE['companyID']) || isset($_GET['cid']) ) {
         die("Invalid request");
     }
     $connection->close();
+}else{
+    $host = $_SERVER['HTTP_HOST']; // e.g., retaj.avenzur.com
+    $parts = explode('.', $host);
+
+    // Check for subdomain existence
+    if (count($parts) > 2) {
+        $company_name = $parts[0]; // 'retaj'
+    } else {
+        die("Invalid Company");
+    }
+
+    $sql = "SELECT * FROM sma_multi_company m 
+                Inner Join `sma_dbs` d 
+                    ON d.id = m.db_id 
+            WHERE is_used = 1 AND is_primary <> 1 AND m.company = {$company_name} limit 1";
+    $result = $connection->query($sql);
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        $username = trim($row["db_user"]);
+        $password = trim($row["db_pass"]);
+        $database = trim($row["db_name"]);
+    }
+
+    $connection->close();
 }
 
 // Connection to Primary DB End
