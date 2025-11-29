c71908a66ca0493f15de22fa5e9a33a51bb2f370
Faisal Abbas <faisalabbas67@gmail.com>
2025-11-29 13:50:25 +0300

    quotes permission

--- themes/blue/admin/views/quotes/pdf.php
+++ themes/blue/admin/views/quotes/pdf.php
@@ -185,11 +185,11 @@
                             <td style="text-align:right;"><?= $this->sma->formatNumber($row->totalbeforevat, null); ?></td>
                             <?php
                             $vat_value = 0;
-                            if ($Settings->tax1 && $row->item_tax > 0) {
+                            //if ($Settings->tax1 && $row->item_tax > 0) {
                                 $vat_value = $this->sma->formatNumber($row->item_tax);
                                 echo '<td style="text-align:right; vertical-align:middle;">' . ($row->item_tax != 0 ?  ($Settings->indian_gst ? $row->tax : $row->tax_code)  : '') . '</td>';
                                 echo '<td>'.$this->sma->formatNumber($row->tax).'</td>';
-                            }
+                            //}
                             ?>
                             <td style="text-align:right; vertical-align:middle;"><?= $this->sma->formatMoney($row->main_net); ?></td>
                         </tr>
