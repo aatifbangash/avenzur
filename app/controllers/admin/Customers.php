368893085d0504357a0ccf111a23dbb363859b31
Faisal Abbas <fabbas@avenzur.com>
2026-02-09 12:36:12 +0300

    customer paid invoices fix

--- app/controllers/admin/Customers.php
+++ app/controllers/admin/Customers.php
@@ -869,7 +869,7 @@ class Customers extends MY_Controller
     }*/
 
     public function reconcile_customer_payments(){
-        $customer_id = 191;
+        $customer_id = 140;
 
         $this->db->trans_begin();
         
